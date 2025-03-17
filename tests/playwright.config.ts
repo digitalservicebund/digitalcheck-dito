c011981e7d863961385a1d9bb5a07513bd11f16d
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-03-17 16:21:04 +0100

    Revert "Test dev server tests again"
    This reverts commit d0ca34abc943c3944dc538d8968c7566108b30fc.

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -74,7 +74,7 @@ const config: PlaywrightTestConfig = {
   ],
   projects: defaultProjects,
   webServer: {
-    command: "npm run dev -- --port 5172",
+    command: "npm run build && NODE_ENV=development PORT=5172 npm run start",
     port: 5172,
     timeout: parseInt(process.env.WAIT_ON_TIMEOUT ?? `${60 * 1000}`),
   },
