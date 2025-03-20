cbb92db7244ba4376b2dc6b9048fb5f2d99f7db0
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-03-20 13:05:46 +0100

    Remove angie from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-import angiePlugin from "@digitalservice4germany/angie";
 import tailwindPreset from "@digitalservice4germany/style-dictionary/tailwind";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [tailwindPreset],
-  plugins: [angiePlugin],
   theme: {
     extend: {
       fontFamily: {
