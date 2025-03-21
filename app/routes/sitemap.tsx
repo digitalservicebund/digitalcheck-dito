5679576611531776f87b91db3a45952e133c93b9
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-03-21 13:12:41 +0100

    Improve sitemap margins

--- app/routes/sitemap.tsx
+++ app/routes/sitemap.tsx
@@ -50,7 +50,7 @@ export default function Sitemap(): ReactNode {
     // [&_li>ul] styles the nested ul elements
     <ul className="list-unstyled [&_li>ul]:ml-8 [&_li>ul]:border-l [&_li>ul]:border-gray-400 [&_li>ul]:pl-8">
       {routes.map((route) => (
-        <li key={route.url} className="my-8 mb-4">
+        <li key={route.url} className="mb-4 [&_li]:my-8">
           <a href={route.url} className="text-link hover:underline">
             {route.title}
           </a>
