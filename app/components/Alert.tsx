20a250cde042140e52a86d104a7ba7ff0c620af1
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-03-21 11:36:01 +0100

    Refactor css components and utilities
    - Fix button sizing issue
    - Rename max-w-prose to max-w-a11y

--- app/components/Alert.tsx
+++ app/components/Alert.tsx
@@ -55,7 +55,7 @@ const Alert = ({
   return (
     <div
       className={twJoin(
-        "ds-stack ds-stack-8 max-w-prose scroll-my-40 border-l-2 p-16 shadow-md",
+        "ds-stack ds-stack-8 max-w-a11y scroll-my-40 border-l-2 p-16 shadow-md",
         backgroundColor,
         borderColor,
       )}
