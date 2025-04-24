26fbbb6b0bc409a991fed7efc65f9f89f480f6eb
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-04-24 13:48:52 +0200

    Improve ProgressBar HTML and a11y

--- app/components/ProgressBar.tsx
+++ app/components/ProgressBar.tsx
@@ -1,38 +1,42 @@
 import { useLocation } from "react-router";
-import Background from "~/components/Background";
 import { progress } from "~/resources/content/progress";
 import twMerge from "~/utils/tailwindMerge";
 
 export default function Progress() {
   const { pathname } = useLocation();
   return (
-    <Background backgroundColor="midBlue">
-      <div className="flex items-center justify-center sm:gap-24">
-        {progress.map((item, index) => (
-          <div
-            key={item.label}
-            className={twMerge(
-              "flex items-center px-12 pt-8 pb-4 text-blue-800 max-md:flex-col md:border-b-4",
-              pathname.startsWith(item.prefix)
-                ? "border-blue-800 font-bold"
-                : "border-transparent",
-            )}
-          >
-            <div
+    <nav
+      className="flex justify-center bg-blue-300 sm:gap-24"
+      aria-label="Digitalcheck-Fortschritt"
+    >
+      <ol className="flex list-none">
+        {progress.map((item, index) => {
+          const isActive = pathname.startsWith(item.prefix);
+          return (
+            <li
+              key={item.label}
+              aria-current={isActive ? "step" : undefined}
               className={twMerge(
-                "mr-8 flex h-24 w-24 items-center justify-center rounded-full border-2",
-                pathname.startsWith(item.prefix)
-                  ? "border-blue-800 bg-blue-800 text-white"
-                  : "border-white",
+                "mb-0 flex px-12 pt-8 pb-4 text-blue-800 max-md:flex-col md:border-b-4",
+                isActive ? "border-blue-800 font-bold" : "border-transparent",
               )}
             >
-              {index + 1}
-            </div>
-            <span className="hidden md:block">{item.label}</span>
-            <span className="md:hidden">{item.labelShort}</span>
-          </div>
-        ))}
-      </div>
-    </Background>
+              <div
+                className={twMerge(
+                  "mr-8 flex h-24 w-24 items-center justify-center rounded-full border-2",
+                  isActive
+                    ? "border-blue-800 bg-blue-800 text-white"
+                    : "border-white",
+                )}
+              >
+                {index + 1}
+              </div>
+              <span className="hidden md:block">{item.label}</span>
+              <span className="md:hidden">{item.labelShort}</span>
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
   );
 }
