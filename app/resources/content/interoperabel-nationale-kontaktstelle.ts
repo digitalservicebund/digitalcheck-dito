4d68926dd193b70e8a39b46f205e5adb497d8611
lucas <l.solcher@gmail.com>
2025-05-06 13:34:44 +0200

    Add tabs spoc page

--- app/resources/content/interoperabel-nationale-kontaktstelle.ts
+++ app/resources/content/interoperabel-nationale-kontaktstelle.ts
@@ -17,6 +17,7 @@ export const spoc = {
   `,
   landscape: {
     id: "landschaft",
+    tabName: "Überblick",
     headline: "Nationale Umsetzung der EU Interoperabilitätsverordnung",
     content: dedent`
       Seit Anfang 2025 muss die Verordnung für ein interoperables Europa ([2024/903](https://eur-lex.europa.eu/legal-content/DE/TXT/?uri=CELEX:32024R0903)) in den EU Mitgliedstaaten verpflichtend umgesetzt werden.
@@ -134,6 +135,7 @@ export const spoc = {
   },
   states: {
     id: "landesebene",
+    tabName: "Digitalcheck nachnutzen",
     headline:
       "Den Digitalcheck mit Interoperabilitätsanforderungen auf Landesebene nutzen",
     sections: [
@@ -151,6 +153,7 @@ export const spoc = {
   },
   contact: {
     id: "kontakt",
+    tabName: "Service & Kontakt",
     headline: "Service und Kontakt",
     sections: [
       {
--- app/routes/interoperabel.nationale-kontaktstelle.tsx
+++ app/routes/interoperabel.nationale-kontaktstelle.tsx
@@ -5,10 +5,10 @@ import Header from "~/components/Header";
 import Heading from "~/components/Heading";
 import ImageZoomable from "~/components/ImageZoomable";
 import InfoBox from "~/components/InfoBox.tsx";
-import LinkListBox from "~/components/LinkListBox";
 import { BulletList } from "~/components/List";
 import { ListItemProps } from "~/components/ListItem";
 import RichText from "~/components/RichText";
+import Tabs, { type TabItem } from "~/components/Tabs.tsx";
 import { spoc } from "~/resources/content/interoperabel-nationale-kontaktstelle";
 import { ROUTE_INTEROPERABILITY_SPOC } from "~/resources/staticRoutes";
 import prependMetaTitle from "~/utils/metaTitle";
@@ -27,49 +27,11 @@ export default function SPOC() {
       look: "ds-subhead",
     },
   }));
-
-  return (
-    <>
-      <Background backgroundColor="blue">
-        <Container>
-          <Header
-            heading={{
-              tagName: "h1",
-              text: spoc.headline,
-            }}
-            content={{
-              markdown: spoc.content,
-              className: "md:text-2xl",
-            }}
-          ></Header>
-          <LinkListBox
-            links={[
-              {
-                id: spoc.landscape.id,
-                title: spoc.landscape.headline,
-              },
-              {
-                id: spoc.responsibilities.id,
-                title: spoc.responsibilities.headline,
-              },
-              {
-                id: spoc.timeline.id,
-                title: spoc.timeline.headline,
-              },
-              {
-                id: spoc.states.id,
-                title: spoc.states.headline,
-              },
-              {
-                id: spoc.contact.id,
-                title: spoc.contact.headline,
-              },
-            ]}
-          />
-        </Container>
-      </Background>
-      <div id={spoc.landscape.id}>
-        <Container>
+  const tabsData: TabItem[] = [
+    {
+      title: spoc.landscape.tabName,
+      content: (
+        <>
           <Heading
             tagName="h2"
             text={spoc.landscape.headline}
@@ -89,55 +51,58 @@ export default function SPOC() {
             className="mb-48"
             markdown={spoc.landscape.contentAfter.outro}
           />
-        </Container>
-      </div>
-      <div id={spoc.responsibilities.id}>
-        <Background backgroundColor="blue">
-          <Container>
+          <Background
+            backgroundColor="blue"
+            className="relative left-1/2 w-screen -translate-x-1/2"
+          >
+            <Container>
+              <Heading
+                tagName="h2"
+                text={spoc.responsibilities.headline}
+                className="mb-8"
+              />
+              <RichText
+                markdown={spoc.responsibilities.content}
+                className="mb-48"
+              />
+            </Container>
+          </Background>
+          <Container className="px-0">
             <Heading
               tagName="h2"
-              text={spoc.responsibilities.headline}
+              text={spoc.timeline.headline}
               className="mb-8"
             />
-            <RichText
-              markdown={spoc.responsibilities.content}
-              className="mb-48"
+            <BulletList
+              items={timelineItems}
+              className="rotate-arrow-bottom mb-48"
             />
           </Container>
-        </Background>
-      </div>
-      <div id={spoc.timeline.id}>
-        <Container>
-          <Heading
-            tagName="h2"
-            text={spoc.timeline.headline}
-            className="mb-8"
-          />
-          <BulletList
-            items={timelineItems}
-            className="rotate-arrow-bottom mb-48"
-          />
-        </Container>
-      </div>
-      <div id={spoc.states.id}>
-        <Background backgroundColor="blue">
-          <Container>
-            <Heading tagName="h2" text={spoc.states.headline} />
-            {spoc.states.sections.map((section) => (
-              <div key={section.headline} className="last:mb-48">
-                <Heading
-                  tagName="h3"
-                  text={section.headline}
-                  className="mt-32 mb-8"
-                />
-                <RichText markdown={section.content} />
-              </div>
-            ))}
-          </Container>
-        </Background>
-      </div>
-      <div id={spoc.contact.id}>
-        <Container>
+        </>
+      ),
+    },
+    {
+      title: spoc.states.tabName,
+      content: (
+        <>
+          <Heading tagName="h2" text={spoc.states.headline} />
+          {spoc.states.sections.map((section) => (
+            <div key={section.headline} className="last:mb-48">
+              <Heading
+                tagName="h3"
+                text={section.headline}
+                className="mt-32 mb-8"
+              />
+              <RichText markdown={section.content} />
+            </div>
+          ))}
+        </>
+      ),
+    },
+    {
+      title: spoc.contact.tabName,
+      content: (
+        <>
           <Heading tagName="h2" text={spoc.contact.headline} />
           {spoc.contact.sections.map((section) => (
             <div key={section.headline} className="last:mb-48">
@@ -149,8 +114,30 @@ export default function SPOC() {
               <RichText markdown={section.content} />
             </div>
           ))}
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <Background backgroundColor="blue">
+        <Container>
+          <Header
+            heading={{
+              tagName: "h1",
+              text: spoc.headline,
+            }}
+            content={{
+              markdown: spoc.content,
+              className: "md:text-2xl",
+            }}
+          ></Header>
         </Container>
-      </div>
+      </Background>
+      <Container className="pt-0">
+        <Tabs tabs={tabsData} />
+      </Container>
     </>
   );
 }
