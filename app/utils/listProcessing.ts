2f8063a55a387ff33577b0d383830bebf026a07a
lucas <l.solcher@gmail.com>
2025-05-22 09:44:50 +0200

    Add comment

--- app/utils/listProcessing.ts
+++ app/utils/listProcessing.ts
@@ -3,6 +3,15 @@ import type { ListItemProps } from "~/components/ListItem.tsx";
 import { methods } from "~/resources/content/methoden.ts";
 import type { ContentRenderer } from "~/routes/methoden._index.tsx";
 
+/**
+ * Maps method step data to ListItemProps, optionally rendering content.
+ * Used by /methoden and /grundlagen/methoden
+ *
+ * @param showContent Boolean flag to determine if content should be rendered.
+ * @param renderContentFn Optional function to render the content for an item.
+ * Required if showContent is true.
+ * @returns An array of ListItemProps.
+ */
 export const methodStepsItems = (
   showContent: boolean,
   renderContentFn?: ContentRenderer,
