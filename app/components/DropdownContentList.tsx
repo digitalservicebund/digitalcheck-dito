8223cdcf35cf9190566c08b20c56eff537ca7694
basti.paintner <basti.paintner@digitalservice.bund.de>
2025-05-23 09:07:27 +0200

    Add page banner

--- app/components/DropdownContentList.tsx
+++ app/components/DropdownContentList.tsx
@@ -1,6 +1,7 @@
 import { NavLink, useLocation } from "react-router";
 import twMerge from "~/utils/tailwindMerge";
 import { normalizePathname } from "~/utils/utilFunctions";
+import Label from "./Label";
 
 export type ActiveBehavior = "noHighlight" | "exactMatch";
 
@@ -37,9 +38,6 @@ export default function DropdownContentList({
     isMobile && "py-8 pr-8 pl-16 border-b-[1px] border-gray-600",
   );
 
-  const newLabelStyle =
-    "ds-label-02-reg mr-8 rounded-md bg-blue-300 px-[4px] py-[2px] text-[#004B76]";
-
   const mapDataToItems = (option: DropdownItemProps, index: number) => {
     return (
       <li key={option.href || option.title || index}>
@@ -72,9 +70,7 @@ export default function DropdownContentList({
                 )}
               >
                 <div className="ds-label-02-reg lg:ds-label-01-bold">
-                  {option.isNewTitle && (
-                    <span className={newLabelStyle}>NEU</span>
-                  )}
+                  {option.isNewTitle && <Label className="mr-8">NEU</Label>}
                   {itemNumber && <span className="mr-4">{itemNumber}. </span>}
                   {option.title}
                 </div>
@@ -83,7 +79,7 @@ export default function DropdownContentList({
                 )}
                 {option.newContent && (
                   <div className="max-lg:hidden">
-                    <span className={newLabelStyle}>NEU</span>
+                    <Label className="mr-8">NEU</Label>
                     <span>{option.newContent}</span>
                   </div>
                 )}
