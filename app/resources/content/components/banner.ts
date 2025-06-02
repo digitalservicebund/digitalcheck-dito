e83d4401ef770300db2472513e9737e9d6c30a94
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-06-02 17:22:14 +0200

    Change Banner text

--- app/resources/content/components/banner.ts
+++ app/resources/content/components/banner.ts
@@ -1,10 +1,7 @@
-import { dedent } from "~/utils/dedentMultilineStrings";
-
 export const banner = {
   title: "Der Digitalcheck bis zur Sommerpause",
   titleLabel: "AKTUELL",
   ariaLabel: "Hinweis Aktuell",
-  content: dedent`
-    Wir überarbeiten derzeit unsere Angebote und stellen unser Vorgehen vorübergehend um. Bis zur Sommerpause gilt daher eine Übergangsregelung: Nutzen Sie nach der Vorprüfung den **vereinfachten Methodenablauf** und die neue **Word-Vorlage** für die Dokumentation.
-  `,
+  content:
+    "Wir überarbeiten derzeit unsere Angebote. Bis zur Sommerpause gilt daher eine Übergangsregelung: Nutzen Sie nach der Vorprüfung den **vereinfachten Methodenablauf** und die neue **Word-Vorlage** für die Dokumentation.",
 };
