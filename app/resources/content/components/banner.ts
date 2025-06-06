350e0ed7d9a135ec263597b88665eb1494ff7796
basti.paintner <basti.paintner@digitalservice.bund.de>
2025-06-06 09:52:08 +0200

    Replace link in banner

--- app/resources/content/components/banner.ts
+++ app/resources/content/components/banner.ts
@@ -1,11 +1,11 @@
 import {
   ROUTE_DOCUMENTATION,
-  ROUTE_FUNDAMENTALS_METHODS,
   ROUTE_FUNDAMENTALS_PRINCIPLES,
+  ROUTE_METHODS,
 } from "~/resources/staticRoutes";
 
 export const banner = {
   title: "Nutzen Sie jetzt den aktualisierten Digitalcheck",
   ariaLabel: "Hinweis Aktuell",
-  content: `Das ist neu, um Sie noch besser in Ihren Regelungsvorhaben zu unterstützen: [Einfach nutzbare Prinzipien](${ROUTE_FUNDAMENTALS_PRINCIPLES.url}), [vereinfachter Methodenablauf](${ROUTE_FUNDAMENTALS_METHODS.url}) und die neue [Word-Vorlage](${ROUTE_DOCUMENTATION.url}) für die Dokumentation.`,
+  content: `Das ist neu, um Sie noch besser in Ihren Regelungsvorhaben zu unterstützen: [Einfach nutzbare Prinzipien](${ROUTE_FUNDAMENTALS_PRINCIPLES.url}), [vereinfachter Methodenablauf](${ROUTE_METHODS.url}) und die neue [Word-Vorlage](${ROUTE_DOCUMENTATION.url}) für die Dokumentation.`,
 };
