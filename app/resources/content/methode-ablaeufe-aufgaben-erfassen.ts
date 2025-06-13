b3073c0b04a192713710819da424a0d6d117630b
basti.paintner <basti.paintner@digitalservice.bund.de>
2025-06-13 13:44:19 +0200

    Refactor don't use logig in resources

--- app/resources/content/methode-ablaeufe-aufgaben-erfassen.ts
+++ app/resources/content/methode-ablaeufe-aufgaben-erfassen.ts
@@ -4,218 +4,119 @@ import {
   StickyNote2Outlined,
   SupportOutlined,
 } from "@digitalservicebund/icons";
-import { CardProps } from "~/components/Card";
-import { HighlightBoxProps } from "~/components/HighlightBox";
-import { InfoBoxProps } from "~/components/InfoBox";
+import { ButtonProps } from "~/components/Button";
 import { dedent } from "~/utils/dedentMultilineStrings";
 import {
   ROUTE_METHODS_PRINCIPLES,
   ROUTE_METHODS_TASKS_PROCESSES_POWERPOINT_PPTX,
 } from "../staticRoutes";
 
-export type ContentVisualisation = {
-  element: "visualisation";
-  documentId: string;
-  plausibleEventName: string;
-};
-
-type ContentItemHighlightBox = {
-  element: "highlightBox";
-  props: HighlightBoxProps;
-};
-
-type ContentItemInfoBox = {
-  element: "infoBox";
-  props: InfoBoxProps;
-};
-
-type ContentItemCard = {
-  element: "card";
-  props: CardProps;
-};
-
-export type ContentItem =
-  | ContentVisualisation
-  | ContentItemInfoBox
-  | ContentItemHighlightBox
-  | ContentItemCard;
-
-type TabItem = {
-  title: string;
-  content: ContentItem[];
-  plausibleEventName: string;
-};
-
-type MethodsTasksProcesses = {
-  title: string;
-  subtitle: string;
-  tabs: TabItem[];
-  furtherSteps: InfoBoxProps;
-  support: InfoBoxProps;
-};
-
-export const methodsTasksProcesses: MethodsTasksProcesses = {
+export const methodsTasksProcesses = {
   title: "Aufgaben und Abläufe visualisieren",
   subtitle: dedent`
     Um Ihre Regelung wirkungsvoll umzusetzen, braucht es ein klares Bild der bestehenden **Abläufe**.
     Holen Sie sich dazu Unterstützung von den umsetzenden **Akteurinnen und Akteuren**. Ein Flussdiagramm hilft, Prozesse sichtbar zu machen und die Digitaltauglichkeit einzuschätzen.
   `,
-  tabs: [
-    {
-      title: "Intro",
-      plausibleEventName: "Tab+Bar.Intro",
-      content: [
-        {
-          element: "infoBox",
-          props: {
-            separator: false,
-            items: [
-              {
-                headline: {
-                  text: "Prozesse sichtbar machen",
-                  tagName: "h2",
-                },
-                content: dedent`
-                Abläufe lassen sich auf verschiedene Weise visualisieren – etwa durch Nutzerreisen, Entscheidungsbäume, Datenflüsse oder Prozessdiagramme.
-                Ein guter Einstieg in Prozessdiagramme ist das **Flussdiagramm**:
-                Sie bilden Prozesse als aufeinanderfolgende Schritte ab. Man sieht, wer welchen Teil des Prozesses übernimmt und Verantwortlichkeiten wechseln.
-            `,
-              },
-            ],
-          },
-        },
-        {
-          element: "visualisation",
-          documentId: "qyh09ifld7r3a8z8h2pyhgom",
-          plausibleEventName: "Content.Intro.Beispiel+Entscheidungsbaum",
-        },
-      ],
+  intro: {
+    title: "Intro",
+    plausibleEventName: "Tab+Bar.Intro",
+    visibility: {
+      headline: "Prozesse sichtbar machen",
+      content: dedent`
+        Abläufe lassen sich auf verschiedene Weise visualisieren – etwa durch Nutzerreisen, Entscheidungsbäume, Datenflüsse oder Prozessdiagramme.
+        Ein guter Einstieg in Prozessdiagramme ist das **Flussdiagramm**:
+        Sie bilden Prozesse als aufeinanderfolgende Schritte ab. Man sieht, wer welchen Teil des Prozesses übernimmt und Verantwortlichkeiten wechseln.
+      `,
     },
-    {
-      title: "Anleitung",
-      plausibleEventName: "Tab+Bar.Anleitung",
-      content: [
-        {
-          element: "infoBox",
-          props: {
-            separator: false,
-            items: [
-              {
-                headline: {
-                  text: "Abläufe mit dem Flussdiagramm erfassen",
-                  tagName: "h2",
-                },
-                content: dedent`
-                  Für Ihr Regelungsvorhaben können Sie ein Flussdiagramm erstellen, um dem gesamten Umsetzungsprozess oder einen spezifischen Arbeitsablauf visuell darzustellen.
+    example: {
+      documentId: "qyh09ifld7r3a8z8h2pyhgom",
+      plausibleEventName: "Content.Intro.Beispiel+Entscheidungsbaum",
+    },
+  },
 
-                  Der Überblick lohnt sich auch bei scheinbar einfachen Abläufen:
-                  - **Fehlende** Verbindungen oder **unerwartete** Abhängigkeiten werden **sichtbar**.
-                  - Sie erfahren, auf welchen **bestehenden Abläufen Sie aufbauen** können.
-                `,
-              },
-              {
-                headline: {
-                  text: "So visualisieren Sie den Umsetzungsprozess in drei Schritten",
-                  tagName: "h3",
-                },
-                content: dedent`
-                  Eine ausführliche Anleitung finden Sie in unserer PowerPoint-Vorlage “**Anleitung Abläufe und Aufgaben erfassen**”.
-                  
-                  1. **Akteure identifizieren:** Bestimmen Sie alle beteiligten Personen, Organisationen und IT-Systeme.
-                  2. **Start/Ende festlegen:**  Legen Sie den Beginn und das Ende des Prozesses aus der Sicht der Bürgerinnen und Bürger oder der umsetzenden Behörden fest.
-                  3. **Prozess visualisieren:** Ordnen Sie Schritte logisch an und verbinden Sie Symbole mit Pfeilen.
-                `,
-              },
-            ],
-          },
-        },
-        {
-          element: "highlightBox",
-          props: {
-            items: [
-              {
-                badge: { text: "Tipp", Icon: LayersOutlined },
-                content: dedent`
-                  Am schnellsten sind Sie, wenn Sie eine Papierskizze machen, bevor Sie am Computer arbeiten. Starten Sie mit reduzierten Informationen und ergänzen Sie nach und nach Details in Ihrem Flussdiagramm. **Wichtig ist das Verständnis, nicht die Ästhetik**.
-                      
-                  Für die digitale Ausarbeitung eigenen sich **Conceptboard**, **draw.io** oder **Microsoft PowerPoint**. Fragen Sie nach geeigneten Programmen und Ansprechpersonen, zum Beispiel Kolleginnen und Kollegen oder in der Z-Abteilung.
-                `,
-              },
-            ],
-          },
-        },
-        {
-          element: "card",
-          props: {
-            badge: { text: "Vorlage", Icon: StickyNote2Outlined },
-            heading: {
-              text: "Anleitung Abläufe und Aufgaben erfassen",
-              tagName: "h3",
-            },
-            content: {
-              markdown:
-                "Eine Powerpoint-Vorlage (PPT) mit einer Schritt für Schritt Anleitung zum Download finden Sie hier. Sie können diese ausdrucken oder an Ihrem Computer bearbeiten. Sofern in Ihrem Ressort Conceptboard genutzt werden kann, können Sie sich auch eine Kopie dieser Conceptboard-Vorlage erstellen. Für letzteres können Sie einen Gast-Zugang anlegen.",
-            },
-            buttons: [
-              {
-                href: ROUTE_METHODS_TASKS_PROCESSES_POWERPOINT_PPTX.url,
-                text: "PPT-Vorlage runterladen",
-                plausibleEventName: "Content.Anleitung.Link+Powerpoint+Vorlage",
-              },
-              {
-                href: "https://bmas.de.conceptboard.com/board/qh33-xcny-usde-7nc6-82cy",
-                text: "Conceptboard-Vorlage",
-                look: "tertiary",
-                plausibleEventName:
-                  "Content.Anleitung.Link+Conceptboard+Vorlage",
-              },
-            ],
-            image: {
-              url: "/images/beispielflussdiagram.png",
-            },
-          },
-        },
-      ],
+  anleitung: {
+    title: "Anleitung",
+    plausibleEventName: "Tab+Bar.Anleitung",
+
+    ablaufe: {
+      headline: "Abläufe mit dem Flussdiagramm erfassen",
+      content: dedent`
+        Für Ihr Regelungsvorhaben können Sie ein Flussdiagramm erstellen, um dem gesamten Umsetzungsprozess oder einen spezifischen Arbeitsablauf visuell darzustellen.
+
+        Der Überblick lohnt sich auch bei scheinbar einfachen Abläufen:
+        - **Fehlende** Verbindungen oder **unerwartete** Abhängigkeiten werden **sichtbar**.
+        - Sie erfahren, auf welchen **bestehenden Abläufen Sie aufbauen** können.
+      `,
     },
-  ],
-  furtherSteps: {
-    items: [
-      {
-        badge: {
-          text: "So geht es weiter",
-          Icon: ArrowCircleRightOutlined,
+
+    visualisierung: {
+      headline: "So visualisieren Sie den Umsetzungsprozess in drei Schritten",
+      content: dedent`
+        Eine ausführliche Anleitung finden Sie in unserer PowerPoint-Vorlage “**Anleitung Abläufe und Aufgaben erfassen**”.
+        
+        1. **Akteure identifizieren:** Bestimmen Sie alle beteiligten Personen, Organisationen und IT-Systeme.
+        2. **Start/Ende festlegen:**  Legen Sie den Beginn und das Ende des Prozesses aus der Sicht der Bürgerinnen und Bürger oder der umsetzenden Behörden fest.
+        3. **Prozess visualisieren:** Ordnen Sie Schritte logisch an und verbinden Sie Symbole mit Pfeilen.
+      `,
+    },
+
+    tipp: {
+      badge: { text: "Tipp", Icon: LayersOutlined },
+      content: dedent`
+        Am schnellsten sind Sie, wenn Sie eine Papierskizze machen, bevor Sie am Computer arbeiten. Starten Sie mit reduzierten Informationen und ergänzen Sie nach und nach Details in Ihrem Flussdiagramm. **Wichtig ist das Verständnis, nicht die Ästhetik**.
+            
+        Für die digitale Ausarbeitung eigenen sich **Conceptboard**, **draw.io** oder **Microsoft PowerPoint**. Fragen Sie nach geeigneten Programmen und Ansprechpersonen, zum Beispiel Kolleginnen und Kollegen oder in der Z-Abteilung.
+      `,
+    },
+
+    vorlage: {
+      badge: { text: "Vorlage", Icon: StickyNote2Outlined },
+      heading: "Anleitung Abläufe und Aufgaben erfassen",
+      content:
+        "Eine Powerpoint-Vorlage (PPT) mit einer Schritt für Schritt Anleitung zum Download finden Sie hier. Sie können diese ausdrucken oder an Ihrem Computer bearbeiten. Sofern in Ihrem Ressort Conceptboard genutzt werden kann, können Sie sich auch eine Kopie dieser Conceptboard-Vorlage erstellen. Für letzteres können Sie einen Gast-Zugang anlegen.",
+      buttons: [
+        {
+          href: ROUTE_METHODS_TASKS_PROCESSES_POWERPOINT_PPTX.url,
+          text: "PPT-Vorlage runterladen",
+          plausibleEventName: "Content.Anleitung.Link+Powerpoint+Vorlage",
         },
-        headline: {
-          text: "Finden Sie konkrete Möglichkeiten der Digitalisierung",
-          tagName: "h3",
+        {
+          href: "https://bmas.de.conceptboard.com/board/qh33-xcny-usde-7nc6-82cy",
+          text: "Conceptboard-Vorlage",
+          look: "tertiary",
+          plausibleEventName: "Content.Anleitung.Link+Conceptboard+Vorlage",
         },
-        content:
-          "Mit den Prinzipien für Digitaltaugliche Gesetzgebung bekommen Sie auch konkrete Hinweise darauf, worauf Sie achten müssen.",
-        buttons: [
-          {
-            href: ROUTE_METHODS_PRINCIPLES.url,
-            text: "Prinzipien nutzen",
-            look: "tertiary",
-            plausibleEventName: "Content.Link+Prinzipien+Nutzen",
-          },
-        ],
+      ] as ButtonProps[],
+      image: {
+        url: "/images/beispielflussdiagram.png",
       },
-    ],
+    },
+  },
+
+  furtherSteps: {
+    badge: {
+      text: "So geht es weiter",
+      Icon: ArrowCircleRightOutlined,
+    },
+    headline: "Finden Sie konkrete Möglichkeiten der Digitalisierung",
+    content:
+      "Mit den Prinzipien für Digitaltaugliche Gesetzgebung bekommen Sie auch konkrete Hinweise darauf, worauf Sie achten müssen.",
+    buttons: [
+      {
+        href: ROUTE_METHODS_PRINCIPLES.url,
+        text: "Prinzipien nutzen",
+        look: "tertiary",
+        plausibleEventName: "Content.Link+Prinzipien+Nutzen",
+      },
+    ] as ButtonProps[],
   },
   support: {
-    items: [
-      {
-        badge: { text: "Unterstützungsangebot", Icon: SupportOutlined },
-        headline: {
-          text: "Visualisierungen gemeinsam erstellen",
-          tagName: "h2",
-        },
-        content: dedent`
-          Der Digitalcheck-Support unterstützt Sie bei der Visualsierung von Abläufen. Wir helfen Ihnen gerne, insbesondere bei komplexen Abläufen.
-          
-          Schreiben Sie uns über [digitalcheck@digitalservice.bund.de](mailto:digitalcheck@digitalservice.bund.de) oder rufen Sie uns an unter [0151/40 76 78 39](tel:015140767839).
-        `,
-      },
-    ],
+    badge: { text: "Unterstützungsangebot", Icon: SupportOutlined },
+    headline: "Visualisierungen gemeinsam erstellen",
+    content: dedent`
+      Der Digitalcheck-Support unterstützt Sie bei der Visualsierung von Abläufen. Wir helfen Ihnen gerne, insbesondere bei komplexen Abläufen.
+      
+      Schreiben Sie uns über [digitalcheck@digitalservice.bund.de](mailto:digitalcheck@digitalservice.bund.de) oder rufen Sie uns an unter [0151/40 76 78 39](tel:015140767839).
+    `,
   },
 };
--- app/routes/methoden.ablaeufe-aufgaben-erfassen.tsx
+++ app/routes/methoden.ablaeufe-aufgaben-erfassen.tsx
@@ -1,5 +1,4 @@
 import { useLoaderData } from "react-router";
-import { twJoin } from "tailwind-merge";
 import Background from "~/components/Background";
 import Card from "~/components/Card";
 import Container from "~/components/Container";
@@ -9,20 +8,17 @@ import InfoBox from "~/components/InfoBox";
 import InterviewBanner from "~/components/InterviewBanner";
 import Tabs, { TabItem } from "~/components/Tabs";
 import VisualisationItem from "~/components/VisualisationItem";
-import {
-  type ContentItem,
-  methodsTasksProcesses,
-} from "~/resources/content/methode-ablaeufe-aufgaben-erfassen";
+import { methodsTasksProcesses } from "~/resources/content/methode-ablaeufe-aufgaben-erfassen";
 import {
   fetchStrapiData,
   visualisationFields,
   Visualisierung,
 } from "~/utils/strapiData.server";
 
-const GET_VISUALISATIONS_QUERY = `
+const GET_VISUALISATION_QUERY = `
 ${visualisationFields}
-query GetVisualisierungen($filters: VisualisierungFiltersInput) {
-  visualisierungen(filters: $filters) {
+query GetVisualisierung($documentId: ID!) {
+  visualisierung(documentId: $documentId) {
     ...VisualisationFields
   }
 }
@@ -30,77 +26,105 @@ query GetVisualisierungen($filters: VisualisierungFiltersInput) {
 
 export const loader = async () => {
   const visualisationsData = await fetchStrapiData<{
-    visualisierungen: Visualisierung[];
-  }>(GET_VISUALISATIONS_QUERY, {
-    filters: {
-      documentId: {
-        in: methodsTasksProcesses.tabs
-          .flatMap(({ content }) =>
-            content.filter((item) => item.element === "visualisation"),
-          )
-          .map(({ documentId }) => documentId),
-      },
-    },
+    visualisierung: Visualisierung;
+  }>(GET_VISUALISATION_QUERY, {
+    documentId: methodsTasksProcesses.intro.example.documentId,
   });
 
   if ("error" in visualisationsData) {
+    console.error(visualisationsData.error);
+
     // eslint-disable-next-line @typescript-eslint/only-throw-error
-    throw new Response(visualisationsData.error, { status: 400 });
+    throw new Response(
+      "Hier hat leider etwas nicht funktioniert, das tut uns Leid.",
+      { status: 500 },
+    );
   }
 
-  return visualisationsData.visualisierungen;
+  return visualisationsData.visualisierung;
 };
 
 export default function Visualization() {
   const visualisationsData = useLoaderData<typeof loader>();
 
-  const createContent = (items: ContentItem[]) => (
-    <div className={twJoin("ds-stack ds-stack-40")}>
-      {items.map((contentItem, i) => {
-        if (contentItem.element === "infoBox") {
-          return (
-            <InfoBox
-              key={(contentItem.props.heading?.text as string) + i}
-              {...contentItem.props}
-            />
-          );
-        } else if (contentItem.element === "highlightBox") {
-          return (
-            <HighlightBox
-              key={(contentItem.props.heading?.text as string) + i}
-              {...contentItem.props}
-            />
-          );
-        } else if (contentItem.element === "card") {
-          return (
-            <Card
-              key={(contentItem.props.heading?.text as string) + i}
-              {...contentItem.props}
-            />
-          );
-        } else if (contentItem.element === "visualisation") {
-          const visualisationsItem = visualisationsData.find(
-            ({ documentId }) => documentId === contentItem.documentId,
-          );
-          if (!visualisationsItem) return <></>;
-
-          return (
+  const tabsData: TabItem[] = [
+    {
+      title: methodsTasksProcesses.intro.title,
+      plausibleEventName: methodsTasksProcesses.intro.plausibleEventName,
+      content: (
+        <div className="ds-stack ds-stack-40">
+          <InfoBox
+            items={[
+              {
+                headline: {
+                  text: methodsTasksProcesses.intro.visibility.headline,
+                  tagName: "h2",
+                },
+                content: methodsTasksProcesses.intro.visibility.content,
+              },
+            ]}
+          />
+          {visualisationsData && (
             <VisualisationItem
-              key={visualisationsItem.Bild.documentId}
-              visualisierung={visualisationsItem}
-              plausibleEventName={contentItem.plausibleEventName}
+              plausibleEventName={
+                methodsTasksProcesses.intro.example.plausibleEventName
+              }
+              visualisierung={visualisationsData}
             />
-          );
-        }
-      })}
-    </div>
-  );
+          )}
+        </div>
+      ),
+    },
 
-  const tabsData: TabItem[] = methodsTasksProcesses.tabs.map((tabData) => ({
-    title: tabData.title,
-    plausibleEventName: tabData.plausibleEventName,
-    content: createContent(tabData.content),
-  }));
+    {
+      title: methodsTasksProcesses.anleitung.title,
+      plausibleEventName: methodsTasksProcesses.anleitung.plausibleEventName,
+      content: (
+        <div className="ds-stack ds-stack-40">
+          <InfoBox
+            separator={false}
+            items={[
+              {
+                headline: {
+                  text: methodsTasksProcesses.anleitung.ablaufe.headline,
+                  tagName: "h2",
+                },
+                content: methodsTasksProcesses.anleitung.ablaufe.content,
+              },
+              {
+                headline: {
+                  text: methodsTasksProcesses.anleitung.visualisierung.headline,
+                  tagName: "h3",
+                },
+                content: methodsTasksProcesses.anleitung.visualisierung.content,
+              },
+            ]}
+          />
+
+          <HighlightBox
+            items={[
+              {
+                badge: methodsTasksProcesses.anleitung.tipp.badge,
+                content: methodsTasksProcesses.anleitung.tipp.content,
+              },
+            ]}
+          />
+
+          <Card
+            image={methodsTasksProcesses.anleitung.vorlage.image}
+            heading={{
+              text: methodsTasksProcesses.anleitung.vorlage.heading,
+              tagName: "h3",
+            }}
+            content={{
+              markdown: methodsTasksProcesses.anleitung.vorlage.content,
+            }}
+            buttons={methodsTasksProcesses.anleitung.vorlage.buttons}
+          />
+        </div>
+      ),
+    },
+  ];
 
   return (
     <>
@@ -125,12 +149,35 @@ export default function Visualization() {
 
       <Container className="ds-stack ds-stack-40">
         <InterviewBanner />
-        <InfoBox {...methodsTasksProcesses.furtherSteps} />
+        <InfoBox
+          items={[
+            {
+              badge: methodsTasksProcesses.furtherSteps.badge,
+              headline: {
+                text: methodsTasksProcesses.furtherSteps.headline,
+                tagName: "h3",
+              },
+              content: methodsTasksProcesses.furtherSteps.content,
+              buttons: methodsTasksProcesses.furtherSteps.buttons,
+            },
+          ]}
+        />
       </Container>
 
       <Background backgroundColor="blue">
         <Container>
-          <InfoBox {...methodsTasksProcesses.support} />
+          <InfoBox
+            items={[
+              {
+                badge: methodsTasksProcesses.support.badge,
+                headline: {
+                  text: methodsTasksProcesses.support.headline,
+                  tagName: "h2",
+                },
+                content: methodsTasksProcesses.support.content,
+              },
+            ]}
+          />
         </Container>
       </Background>
     </>
