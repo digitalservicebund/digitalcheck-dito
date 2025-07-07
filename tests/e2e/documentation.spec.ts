a510de97c768031cf9fdc813a8d5df9ea951f2f9
Merlin Pannewitz <merlin.pannewitz@digitalservice.bund.de>
2025-07-07 16:36:07 +0200

    Add test for documentation file download

---
+++ tests/e2e/documentation.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "@playwright/test";
+import { ROUTE_DOCUMENTATION } from "~/resources/staticRoutes";
+
+test.describe("documentation process", () => {
+  test("downloading documentation document works", async ({ page }) => {
+    await page.goto(ROUTE_DOCUMENTATION.url);
+
+    const downloadPromise = page.waitForEvent("download");
+    await page
+      .getByRole("link", { name: "Dokumentation herunterladen" })
+      .click();
+    const download = await downloadPromise;
+    expect(download.suggestedFilename()).toContain(
+      "Dokumentieren-der-Digitaltauglichkeit_V1-5.docx",
+    );
+  });
+});
