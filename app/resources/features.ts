40afed2545db5fca2a68265a1d12c17a71bda7a8
Merlin Pannewitz <merlin.pannewitz@digitalservice.bund.de>
2025-07-30 13:21:31 +0200

    Revert "Add route throwing error for testing"
    This reverts commit 23e69dcc16de63db9efeaaa53f7048c6527df72e.

--- app/resources/features.ts
+++ app/resources/features.ts
@@ -2,6 +2,4 @@ export const features: Record<string, string> = {
   enableDocumentationPrototype: "digitalcheck.enable-documentation-prototype",
   enableDocumentationPrototypeAlternative:
     "digitalcheck.enable-documentation-prototype-alternative",
-  enableThrowErrorRoute:
-    "digitalcheck.enable-route-throwing-error-for-debugging",
 };
