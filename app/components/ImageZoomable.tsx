5f3c6e87fe56ef15b81a844b71f00cb5af145cb9
basti.paintner <basti.paintner@digitalservice.bund.de>
2025-07-31 10:25:36 +0200

    Use tailwind color classes

--- app/components/ImageZoomable.tsx
+++ app/components/ImageZoomable.tsx
@@ -44,7 +44,7 @@ function ImageZoomable({
         )}
       />
       <ZoomInOutlined
-        className="bg-ds-dark-blue absolute bottom-16 left-16 size-48 p-1 shadow-sm"
+        className="absolute bottom-16 left-16 size-48 bg-blue-800 p-1 shadow-sm"
         fill="white"
       />
     </Link>
