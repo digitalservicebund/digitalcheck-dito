2b9d61ad860ce23ba7c72c5b2d4700ff00cfcbf7
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-08-04 17:42:30 +0200

    Add new landing page

---
+++ app/resources/content/startseite-neu.ts
@@ -0,0 +1,204 @@
+import {
+  ROUTE_DOCUMENTATION,
+  ROUTE_EXAMPLES,
+  ROUTE_INTEROPERABILITY,
+  ROUTE_METHODS,
+  ROUTE_METHODS_PRINCIPLES,
+  ROUTE_PRECHECK,
+} from "~/resources/staticRoutes";
+import { dedent } from "~/utils/dedentMultilineStrings";
+
+export const startseite = {
+  title: "Digitaltaugliche Regelungen erarbeiten",
+  subtitle: dedent`
+    Der Digitalcheck ist eine Prozessbegleitung für Ihr Regelungsvorhaben. Sie
+    stellen damit Digitaltauglichkeit sicher.
+  `,
+  stepByStep: {
+    title: "Schritt für Schritt",
+    procedureLabel: "Vorgehen",
+    durationLabel: "Dauer",
+    resultLabel: "Ergebnis",
+    steps: [
+      {
+        number: 1,
+        title: "Digitalbezug einschätzen",
+        link: {
+          text: "Digitalbezug einschätzen",
+          href: ROUTE_PRECHECK.url,
+          look: "primary" as const,
+        },
+        description:
+          "Sie identifizieren den Digitalbezug und Interoperabilitätsanforderungen für Ihr Vorhaben.",
+        duration: "Wenige Minuten",
+        result:
+          "Identifizierter Digital- und Interoperabilitätsbezug; Ergebnis als E-Mail mit ggf. weiteren notwendigen Normenkontrolle",
+      },
+      {
+        number: 2,
+        title: "Digitaltauglichkeit der Regelung sicherstellen",
+        link: {
+          text: "Regelung erarbeiten",
+          href: ROUTE_METHODS.url,
+          look: "ghost" as const,
+        },
+        description:
+          "Bei einer positiven Vorprüfung wenden Sie die Werkzeuge und Methoden an, die die Digitaltauglichkeit Ihres Vorhabens sicherstellen.",
+        duration: "Punktuell über die gesamte Zeit der Regelungsausarbeitung",
+        result:
+          "Ihre Regelung wird organisatorisch, visualisiert als zur Angebotenen Umsetzung Ihrer Regelung",
+      },
+      {
+        number: 3,
+        title: "Dokumentieren der Digitaltauglichkeit",
+        link: {
+          text: "Digitaltauglichkeit dokumentieren",
+          href: ROUTE_DOCUMENTATION.url,
+          look: "ghost" as const,
+        },
+        description:
+          "Sie dokumentieren, wie Sie Digitaltauglichkeit in Ihrer Regelung sichergestellt haben.",
+        duration: "Ein bis zwei Stunden",
+        result:
+          "Ausgefülltes Word-Dokument, welches Sie dem Normenkontrollrat senden",
+      },
+    ],
+  },
+
+  grundlagen: {
+    title: "Grundlagen zum Digitalcheck",
+    wasIstDigitaltauglichkeit: {
+      title:
+        "Was ist Digitaltauglichkeit? Und wie unterstützt Sie der Digitalcheck dabei?",
+      content:
+        "Digitaltaugliche Regelungen ermöglichen eine rechtlich und technisch reibungslose digitale Umsetzung und fördern die automatisierte Bearbeitung von Verfahrensschritten.",
+      linkText: "Mehr zur Digitaltauglichkeit erfahren",
+    },
+    nationaleNormenkontrolle: {
+      title: "Worauf achtet der Nationale Normenkontrollrat?",
+      content:
+        "Der Nationale Normenkontrollrat (NKR) prüft das Vorhaben auf Digitaltauglichkeit. Mit der Anwendung der **Prinzipien** und Erstellung von **Visualisierungen** kann der Nationale Normenkontrollrat die Digitaltauglichkeit besser und schneller nachvollziehen.",
+      linkText: "Mehr zum NKR erfahren",
+    },
+  },
+
+  visualisierungen: {
+    title: "Das Potential von Visualisierungen nutzen",
+    content:
+      "Visualisierungen helfen kostspieligen Aufbau Ihrer Regelung einfach sichtbar zu machen und Digitaltauglichkeit einzuschätzen.",
+    buttons: [
+      {
+        text: "Zu Visualisierungen",
+        href: "#",
+        look: "tertiary" as const,
+      },
+      { text: "Beispiele", href: "#", look: "ghost" as const },
+    ],
+  },
+
+  prinzipien: {
+    title: "Prinzipien als Grundlage für Digitaltauglichkeit einsetzen",
+    content:
+      "Fünf Prinzipien helfen Ihnen dabei, bei der digitalen Umsetzung ausgeschöpfte und Hindernisse zu erkennen.",
+    buttons: [
+      {
+        text: "Zu den Prinzipien",
+        href: ROUTE_METHODS_PRINCIPLES.url,
+        look: "tertiary" as const,
+      },
+      { text: "Beispiele", href: "#", look: "ghost" as const },
+    ],
+  },
+
+  individuelleExpertise: {
+    title: "Individuelle Digital-Expertise für Ihr Vorhaben kennenlernen",
+    content:
+      "Nutzen Sie unsere persönlichen Unterstützung: Angebote, um Ihr Regelungsvorhaben digitaltauglich zu gestalten und den Prozess erfolgreich durchzuführen.",
+    button: {
+      text: "Angebote kennenlernen",
+      href: "#",
+      look: "tertiary" as const,
+    },
+  },
+
+  quote: {
+    text: "„Der Digitalcheck erscheint mit den Hilfestellungen unkomplizierter als gedacht.“",
+    reference: "**Referentin**<br>aus einem Bundesministerium",
+  },
+
+  dataNotice: {
+    headline: "Ihre Arbeitsstände werden eine Woche lang gespeichert.",
+    content:
+      "Um in dieser Zeit zu Ihrem alten Arbeitsstand zurückzukehren, nutzen Sie bitte denselben Rechner und Browser.",
+  },
+  trainings: {
+    title: "Beispiele für Digitaltauglichkeit",
+    text: dedent`
+      Auf dieser Seite finden Sie Beispiele für digitaltaugliche Regelungsvorhaben. Lassen Sie sich inspirieren durch:
+      - die Umsetzung der 5 Prinzipien in Regelungstexten durch konkrete Formulierungen
+      - geeignete Visualisierungen einzelner Sachverhalte und ganzer Regelungsvorhaben`,
+    link: {
+      text: "Jetzt Beispiele entdecken",
+      href: ROUTE_EXAMPLES.url,
+      look: "link" as const,
+    },
+  },
+  interoperability: {
+    title: "EU-Vorgaben zur Interoperabilität in Regelungen",
+    text: "Seit Januar 2025 tritt die [Verordnung für ein interoperables Europa (EU 2024/903)](https://eur-lex.europa.eu/legal-content/DE/TXT/?uri=CELEX%3A32024R0903) in Kraft. Bestimmte Regelungsvorhaben müssen daher die EU-Anforderungen an Interoperabilität erfüllen. Ob und wie die Verordnung Ihr Vorhaben betrifft, erfahren Sie in unserer [Vorprüfung](/vorpruefung) und auf der Übersichtsseite zur Interoperabilität.",
+    link: {
+      text: "Übersicht zur Interoperabilität",
+      href: ROUTE_INTEROPERABILITY.url,
+      look: "link" as const,
+    },
+  },
+  summary: {
+    tabName: "Was ist Digitaltauglichkeit?",
+    plausibleEventName: "Tab+Bar+Digitaltauglichkeit",
+    title: "Zusammengefasst",
+    items: [
+      {
+        heading: {
+          text: "Was ist Digitaltauglichkeit?",
+        },
+        content: dedent`
+          Regelungen werden vermehrt digital umgesetzt. Beispiele sind eine Gesetzesänderung, mit der ein Papierantrag 
+          durch einen Online-Antrag ersetzt wird oder eine Verordnung, die Änderungen in den IT-Verfahren nachgelagerter 
+          Behörden erfordert.
+          <br class="block content-[''] mb-24!" />
+          Damit die digitale Umsetzung reibungslos klappt, muss die Regelung digitaltauglich gestaltet sein. Das heißt 
+          erstens, dass der digitalen Umsetzung nichts im Wege steht, wie zum Beispiel das persönliche Einreichen von 
+          Dokumenten. Zweitens soll aktiv gefördert werden, dass möglichst viele Schritte von Computern durchgeführt 
+          oder unterstützt werden.`,
+      },
+      {
+        heading: {
+          text: "Digitaltaugliche Regelungen sparen Ressourcen",
+        },
+        content:
+          "Eine gute digitale Umsetzung spart langfristig Zeit und sorgt dafür, dass Ziel und Wirkung des Vorhabens erreicht werden: auf Seiten der Normadressaten und -adressatinnen und auf Seiten der Verwaltung.",
+      },
+      {
+        heading: {
+          text: "Alle Regelungsvorhaben sind betroffen",
+        },
+        content:
+          "Der Digitalcheck gilt für alle Regelungsvorhaben (Gesetze, Verordnungen und Verwaltungsvorschriften), sowohl für neue Vorhaben als auch für Änderungen an bestehenden Regelungen.",
+      },
+    ],
+  },
+  principles: {
+    title: "5 Prinzipien für digitaltaugliche Gesetzgebung",
+    content: [
+      "Prinzip 1: Digitale Kommunikation sicherstellen",
+      "Prinzip 2: Wiederverwendung von Daten und Standards ermöglichen",
+      "Prinzip 3: Datenschutz und Informationssicherheit gewährleisten",
+      "Prinzip 4: Klare Regelungen für eine digitale Ausführung finden",
+      "Prinzip 5: Automatisierung ermöglichen",
+    ],
+    link: {
+      text: "Details und Beispiele",
+      href: ROUTE_METHODS_PRINCIPLES.url,
+    },
+  },
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,167 @@
+import ContactPhoneOutlinedIcon from "@digitalservicebund/icons/ContactPhoneOutlined";
+import FactCheckOutlinedIcon from "@digitalservicebund/icons/FactCheckOutlined";
+import WidgetsOutlinedIcon from "@digitalservicebund/icons/WidgetsOutlined";
 import { Link } from "react-router";
 import Box from "~/components/Box";
+import Button from "~/components/Button";
 import Container from "~/components/Container";
 import Heading from "~/components/Heading";
 import Hero from "~/components/Hero";
 import ImageBox from "~/components/ImageBox.tsx";
+import InfoBox from "~/components/InfoBox";
 import InfoBoxList from "~/components/InfoBoxList";
+import InfoBoxSideBySide from "~/components/InfoBoxSideBySide";
 import { NumberedList } from "~/components/List";
+import RichText from "~/components/RichText";
 import Tabs, { type TabItem } from "~/components/Tabs.tsx";
 import { index } from "~/resources/content/startseite";
+import { startseite } from "~/resources/content/startseite-neu";
+import useFeatureFlag from "~/utils/featureFlags";
+
+const {
+  title,
+  subtitle,
+  stepByStep,
+  grundlagen,
+  individuelleExpertise,
+  quote,
+} = startseite;
 
 export default function Index() {
+  const newLandingPage = useFeatureFlag("digitalcheck.enable-new-landing-page");
+
+  if (newLandingPage) {
+    return (
+      <>
+        <Hero
+          title={title}
+          subtitle={subtitle}
+          className="bg-blue-800 text-white"
+        />
+
+        <Container className="py-80">
+          <Heading tagName="h2" text={stepByStep.title} className="mb-40" />
+
+          <ol className="list-unstyled relative grid max-w-none grid-flow-col md:grid-cols-3 md:grid-rows-[repeat(6,auto)]">
+            <div className="absolute top-1 -right-1 h-16 w-16 -translate-y-1/2 rounded-full bg-blue-300" />
+            {stepByStep.steps.map((step) => (
+              <li
+                key={step.number}
+                className="row-span-6 grid grid-rows-subgrid border-t-3 border-blue-300 pr-40"
+              >
+                <div className="absolute top-1 flex h-40 w-40 -translate-y-1/2 items-center justify-center rounded-full bg-blue-800 font-bold text-white">
+                  {step.number}
+                </div>
+
+                <Heading
+                  tagName="h3"
+                  text={step.title}
+                  className="ds-heading-03-bold mt-40"
+                />
+
+                <Button
+                  href={step.link.href}
+                  look={step.link.look}
+                  className="mt-40 self-center"
+                >
+                  {step.link.text}
+                </Button>
+
+                <div className="mt-40">
+                  <p className="font-bold">{stepByStep.procedureLabel}:</p>
+                  <RichText markdown={step.description} />
+                </div>
+
+                <div className="mt-24 border-t-2 border-gray-400 pt-16">
+                  <p className="font-bold">{stepByStep.durationLabel}:</p>
+                  {step.duration}
+                </div>
+                <div className="mt-24">
+                  <p className="font-bold">{stepByStep.resultLabel}:</p>
+                  {step.result}
+                </div>
+              </li>
+            ))}
+          </ol>
+        </Container>
+
+        <div className="bg-blue-100">
+          <Container className="ds-stack ds-stack-40 py-80">
+            <Heading tagName="h2" text={grundlagen.title} />
+
+            <InfoBox
+              visual={{ type: "icon", Icon: WidgetsOutlinedIcon }}
+              look="highlight"
+              className="bg-white"
+              heading={{
+                tagName: "h3",
+                text: grundlagen.wasIstDigitaltauglichkeit.title,
+              }}
+              content={grundlagen.wasIstDigitaltauglichkeit.content}
+              buttons={[
+                {
+                  text: grundlagen.wasIstDigitaltauglichkeit.linkText,
+                  href: "#",
+                  look: "link",
+                },
+              ]}
+            />
+
+            <InfoBox
+              visual={{ type: "icon", Icon: FactCheckOutlinedIcon }}
+              look="highlight"
+              className="bg-white"
+              heading={{
+                tagName: "h3",
+                text: grundlagen.nationaleNormenkontrolle.title,
+              }}
+              content={grundlagen.nationaleNormenkontrolle.content}
+              buttons={[
+                {
+                  text: grundlagen.nationaleNormenkontrolle.linkText,
+                  href: "#",
+                  look: "link",
+                },
+              ]}
+            />
+          </Container>
+        </div>
+
+        <div className="bg-[url('/images/trainings.jpg')] bg-cover bg-center">
+          <Container className="py-80">
+            <InfoBoxSideBySide>
+              <div />
+              <div />
+            </InfoBoxSideBySide>
+          </Container>
+        </div>
+
+        <Container className="space-y-48 py-80">
+          <InfoBox
+            heading={{
+              tagName: "h2",
+              text: individuelleExpertise.title,
+            }}
+            content={individuelleExpertise.content}
+            buttons={[individuelleExpertise.button]}
+          />
+
+          <InfoBox
+            look="highlight"
+            className="bg-blue-100"
+            visual={{ type: "icon", Icon: ContactPhoneOutlinedIcon }}
+            heading={{
+              tagName: "h3",
+              text: quote.text,
+              className: "ds-heading-03-reg mb-16",
+            }}
+            content={quote.reference}
+          />
+        </Container>
+      </>
+    );
+  }
+
   const tabsData: TabItem[] = [
     {
       title: index.stepByStep.tabName,
