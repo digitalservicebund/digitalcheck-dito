13c1983caf2849bea8db55c6bd612a2714a18ace
Merlin Pannewitz <merlin.pannewitz@digitalservice.bund.de>
2025-08-05 15:51:54 +0200

    Remove unused property from ButtonContainer.tsx

--- app/components/ButtonContainer.tsx
+++ app/components/ButtonContainer.tsx
@@ -2,24 +2,13 @@ import twMerge from "~/utils/tailwindMerge";
 import Button, { type ButtonLinkProps, type ButtonProps } from "./Button";
 
 type ButtonContainerProps = {
-  reverseOrder?: boolean;
   className?: string;
   buttons: (ButtonLinkProps | ButtonProps)[];
 };
 
-const ButtonContainer = ({
-  buttons,
-  reverseOrder,
-  className,
-}: ButtonContainerProps) => {
+const ButtonContainer = ({ buttons, className }: ButtonContainerProps) => {
   return (
-    <div
-      className={twMerge(
-        "flex flex-wrap gap-16",
-        reverseOrder && "flex-wrap-reverse",
-        className,
-      )}
-    >
+    <div className={twMerge("flex flex-wrap gap-16", className)}>
       {buttons.map((button) => (
         <Button key={button.text ?? button.href} {...button} />
       ))}
