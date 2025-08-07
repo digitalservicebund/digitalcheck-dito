442d65cb1388e1d8d70d537e46085e71620e3fb6
basti.paintner <basti.paintner@digitalservice.bund.de>
2025-08-07 13:26:23 +0200

    PR comments and refactoring

---
+++ app/components/Absatz.tsx
@@ -0,0 +1,133 @@
+import { ArrowUpwardOutlined } from "@digitalservicebund/icons";
+import { useContext } from "react";
+import { Link, useLocation } from "react-router";
+import { twJoin } from "tailwind-merge";
+import {
+  PrincipleHighlightContext,
+  PrincipleHighlightProvider,
+} from "~/providers/PrincipleHighlightProvider";
+import { PRINCIPLE_COLORS } from "~/resources/constants";
+import { examples } from "~/resources/content/beispiele";
+import {
+  absatzIdTag,
+  AbsatzWithNumber,
+  explanationID,
+  prependNumberToAbsatz,
+} from "~/utils/paragraphUtils";
+import { Prinzip, PrinzipErfuellung } from "~/utils/strapiData.server";
+import { BlocksRenderer } from "./BlocksRenderer";
+import Heading from "./Heading";
+import PrincipleHighlightModifier from "./PrincipleHighlightModifier";
+
+type AbsatzProps = {
+  absatz: AbsatzWithNumber;
+  principlesToShow: Prinzip[];
+  useAnchorLinks: boolean;
+};
+
+export default function Absatz({
+  absatz,
+  principlesToShow,
+  useAnchorLinks,
+}: Readonly<AbsatzProps>) {
+  const content = prependNumberToAbsatz(absatz);
+
+  return (
+    <PrincipleHighlightProvider
+      absatzId={absatz.id.toString()}
+      principlesToShow={principlesToShow}
+      useAnchorLinks={useAnchorLinks}
+    >
+      <div className="flex flex-col gap-16 md:grid md:grid-cols-[5fr_3fr] md:gap-32 [&_ol]:list-decimal [&_ol_ol]:list-[lower-alpha]">
+        <BlocksRenderer
+          id={absatzIdTag(absatz.id)}
+          className="italic"
+          content={content}
+          modifiers={{
+            underline: PrincipleHighlightModifier,
+          }}
+        />
+        <PrincipleExplanationList absatz={absatz} />
+      </div>
+    </PrincipleHighlightProvider>
+  );
+}
+
+type PrincipleExplanationListProps = {
+  absatz: AbsatzWithNumber;
+};
+
+function PrincipleExplanationList({
+  absatz,
+}: Readonly<PrincipleExplanationListProps>) {
+  return (
+    <div className="flex flex-col gap-8">
+      <Heading
+        tagName="h4"
+        className="ds-label-01-bold"
+        ariaLabel={`${examples.paragraphs.ariaLabelPrefix} ${absatz.number}: ${examples.paragraphs.explanation}`}
+      >
+        {examples.paragraphs.explanation}
+      </Heading>
+      {absatz.PrinzipErfuellungen.map(
+        (erfuellung) =>
+          erfuellung.Prinzip && (
+            <PrincipleExplanation key={erfuellung.id} erfuellung={erfuellung} />
+          ),
+      )}
+    </div>
+  );
+}
+
+type PrincipleExplanationProps = {
+  erfuellung: PrinzipErfuellung;
+};
+
+function PrincipleExplanation({
+  erfuellung,
+}: Readonly<PrincipleExplanationProps>) {
+  const location = useLocation();
+  const { activeHighlight, setActiveHighlight, absatzId } = useContext(
+    PrincipleHighlightContext,
+  );
+  if (!absatzId || !erfuellung.Prinzip) return null;
+  const id = explanationID(absatzId, erfuellung.Prinzip.Nummer);
+
+  // NOTE: The CSS target pseudo-class doesn't work here due to the client side navigation: https://github.com/remix-run/remix/issues/6432.
+  // Using the hash also leads to a hydration mismatch due to the location hash only being available on the client.
+  const shouldHighlight = location.hash === `#${id}`;
+  const color = PRINCIPLE_COLORS[erfuellung.Prinzip.Nummer];
+  const explanationClasses = twJoin(
+    "w-fit max-w-[642px] px-8",
+    color.border,
+    shouldHighlight ? "border-4 p-4" : "border-l-4 p-8",
+  );
+
+  return (
+    <div
+      id={id}
+      className={explanationClasses}
+      aria-description="Beispielerklärung"
+    >
+      <div className="flex content-center gap-8">
+        <BlocksRenderer content={erfuellung.WarumGut} />
+        {shouldHighlight && activeHighlight && (
+          <Link
+            to={`#${activeHighlight}`}
+            className="ds-link-01-bold"
+            aria-label="Zurück zum Text"
+            onClick={() => setActiveHighlight(null)}
+            replace
+          >
+            <ArrowUpwardOutlined className="fill-blue-800" />
+          </Link>
+        )}
+      </div>
+      <p className="text-gray-900">
+        (
+        {`${examples.principleExplanation.principle}: ${erfuellung.Prinzip.Name}`}
+        )
+      </p>
+    </div>
+  );
+}
