7c41709802725f1c1f6df3c81e72349cb3b996a4
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-08-28 17:32:09 +0200

    Optimize breakpoints

--- app/layout/SidebarContainer.tsx
+++ app/layout/SidebarContainer.tsx
@@ -15,10 +15,10 @@ export function SidebarContainer({
   sidebar,
 }: Readonly<SidebarContainerProps>) {
   return (
-    <Container className="sm:pr-0 2xl:ml-[calc((100vw-59rem)/2)] 2xl:max-w-[72rem]">
-      <div className="grid grid-cols-1 gap-y-40 sm:grid-cols-[1fr_minmax(184px,256px)] md:my-40">
-        <div className="space-y-40 md:space-y-80">{children}</div>
-        <div className="order-first ml-20 sm:sticky sm:top-40 sm:order-none sm:ml-0 sm:max-h-[calc(100vh-80px)] sm:self-start sm:overflow-y-auto sm:px-40">
+    <Container className="lg:pr-0 min-[90rem]:ml-[calc((100vw-59rem)/2)] min-[90rem]:max-w-[72rem]">
+      <div className="grid grid-cols-1 gap-y-40 lg:my-40 lg:grid-cols-[1fr_minmax(184px,256px)]">
+        <div className="space-y-40 lg:space-y-80">{children}</div>
+        <div className="order-first ml-20 lg:sticky lg:top-40 lg:order-none lg:ml-0 lg:max-h-[calc(100vh-80px)] lg:self-start lg:overflow-y-auto lg:px-40">
           {sidebar}
         </div>
       </div>
