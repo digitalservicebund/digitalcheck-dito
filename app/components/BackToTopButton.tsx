00a8570dd760c60369faa3880a8d7d4d0017b7aa
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-09-15 14:10:57 +0200

    Adapt back-to-top button

--- app/components/BackToTopButton.tsx
+++ app/components/BackToTopButton.tsx
@@ -1,24 +1,15 @@
 import { ArrowUpward } from "@digitalservicebund/icons";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { twJoin } from "tailwind-merge";
 
 export default function BackToTopButton() {
   const [isVisible, setIsVisible] = useState(false);
 
-  const lastScroll = useRef(0);
-
-  // only show the button if the user has scrolled past three screen heights, and is scrolling up
   const handleScroll = () => {
-    // Show the button when the user has scrolled down more than 300 pixels.
-    const isScrollingUp = lastScroll.current > window.scrollY;
-    lastScroll.current = window.scrollY;
-
-    const scrollThreshold = window.innerHeight * 3;
-    if (window.scrollY > scrollThreshold && isScrollingUp) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
+    // only show the button when the user has scrolled past the fold
+    const scrollThreshold = window.innerHeight;
+    const shouldShowButton = window.scrollY > scrollThreshold;
+    setIsVisible(shouldShowButton);
   };
 
   const scrollToTop = () => {
@@ -40,7 +31,7 @@ export default function BackToTopButton() {
     <button
       onClick={scrollToTop}
       className={twJoin(
-        "ds-label-03-reg fixed right-16 bottom-16 z-50 flex items-center gap-8 rounded-full bg-blue-800 p-8 text-white shadow-md outline-2 outline-blue-500 transition-opacity duration-150 sm:right-24 sm:bottom-24",
+        "ds-label-03-reg fixed right-16 bottom-16 z-50 flex items-center gap-8 rounded-full bg-blue-800 p-8 pr-16 text-white shadow-md outline-2 outline-blue-500 transition-opacity duration-150 sm:right-24 sm:bottom-24",
         isVisible ? "opacity-100" : "pointer-events-none opacity-0",
       )}
       aria-hidden={!isVisible}
