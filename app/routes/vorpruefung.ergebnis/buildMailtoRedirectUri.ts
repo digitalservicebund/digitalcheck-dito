ff26f6d61ad791c7dde098966b4712be8fb783d7
Merlin Pannewitz <merlin.pannewitz@digitalservice.bund.de>
2025-09-17 07:38:15 +0200

    Revert "Fix import.meta.env in E2E"
    This reverts commit 57d483a096aff3ae611aa78c447087b71dc96990.

--- app/routes/vorpruefung.ergebnis/buildMailtoRedirectUri.ts
+++ app/routes/vorpruefung.ergebnis/buildMailtoRedirectUri.ts
@@ -51,21 +51,8 @@ export function buildEmailBody(
  * Optionally prefix mailto: links so that they appear to be a local page. This is so that end-to-end tests, run
  * locally, don't cause the OS's email client to open while running tests.
  */
-const mockMailtoLinks = (function () {
-  // running in browser
-  // TODO: make import.meta.env available in Playwright E2E tests as well
-  if (import.meta.env) {
-    return (
-      import.meta.env.VITE_MOCK_MAILTO_LINKS === "true" && !import.meta.env.PROD
-    );
-  } else {
-    // running SSR
-    return (
-      process.env.MOCK_MAILTO_LINKS === "true" &&
-      process.env.NODE_ENV !== "production"
-    );
-  }
-})();
+const mockMailtoLinks =
+  import.meta.env.VITE_MOCK_MAILTO_LINKS === "true" && !import.meta.env.PROD;
 
 if (mockMailtoLinks) {
   console.warn("Prefixing mailto: links");
