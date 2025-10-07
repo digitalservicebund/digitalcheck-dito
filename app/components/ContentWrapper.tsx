57ca00adb0779687cc0f1fbbeb00294cc52112ca
Merlin Pannewitz <merlin.pannewitz@digitalservice.bund.de>
2025-10-07 09:17:18 +0200

    Refactor ContentWrapper to not use negativ margin

--- app/components/ContentWrapper.tsx
+++ app/components/ContentWrapper.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
+import Tabs from "~/components/Tabs";
 import twMerge from "~/utils/tailwindMerge.ts";
 
 /**
@@ -8,16 +9,21 @@ import twMerge from "~/utils/tailwindMerge.ts";
  * It wraps children in a div with `container` class and vertical margin:
  * - 40px on narrow viewports
  * - 80px on lg and above
- *   - Reduces that back to 40px using negative margin for tab lists
+ *   - exception: if child is Tabs set margin top to 40px instead
  */
 function ContentWrapper({
   children,
   className,
 }: Readonly<{ children: ReactNode; className?: string }>) {
+  const firstChild = React.Children.toArray(children)[0];
+  const isFirstChildTabs =
+    React.isValidElement(firstChild) && firstChild.type === Tabs;
+
   return (
     <div
       className={twMerge(
-        "container my-40 space-y-40 lg:my-80 lg:[&>*>[role=tablist]]:-mt-40",
+        "container my-40 space-y-40 lg:my-80",
+        isFirstChildTabs && "lg:mt-40",
         className,
       )}
     >
