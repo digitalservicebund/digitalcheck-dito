837b54d29d114a00728fb22ef64df635c69a131c
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-10-07 17:24:34 +0200

    Redirect grundlagen 5 principles to methods

--- app/routes/grundlagen.fuenf-prinzipien.tsx
+++ app/routes/grundlagen.fuenf-prinzipien.tsx
@@ -1,57 +1,6 @@
-import { useLoaderData } from "react-router";
-import Hero from "~/components/Hero";
-import MetaTitle from "~/components/Meta";
-import { PrinciplePosterBox } from "~/components/PrinciplePosterBox";
-import TableOfContents from "~/components/TableOfContents";
-import { methodsFivePrinciples } from "~/resources/content/methode-fuenf-prinzipien";
-import { ROUTE_FUNDAMENTALS_PRINCIPLES } from "~/resources/staticRoutes";
-import {
-  fetchStrapiData,
-  GET_PRINZIPS_WITH_EXAMPLES_QUERY,
-  PrinzipWithAspekte,
-} from "~/utils/strapiData.server";
-import { slugify } from "~/utils/utilFunctions";
-import Principle from "./methoden.fuenf-prinzipien/Principle";
+import { redirect } from "react-router";
+import { ROUTE_METHODS_PRINCIPLES } from "~/resources/staticRoutes";
 
-export const loader = async () => {
-  const prinzipData = await fetchStrapiData<{
-    prinzips: PrinzipWithAspekte[];
-  }>(GET_PRINZIPS_WITH_EXAMPLES_QUERY);
-
-  if ("error" in prinzipData) {
-    // eslint-disable-next-line @typescript-eslint/only-throw-error
-    throw new Response(prinzipData.error, { status: 400 });
-  }
-
-  return { prinzips: prinzipData.prinzips };
-};
-
-export default function FivePrinciples() {
-  const { prinzips } = useLoaderData<typeof loader>();
-
-  return (
-    <>
-      <MetaTitle prefix={ROUTE_FUNDAMENTALS_PRINCIPLES.title} />
-      <Hero
-        title={methodsFivePrinciples.grundlagenTitle}
-        subtitle={methodsFivePrinciples.grundlagenSubtitle}
-      >
-        <TableOfContents
-          heading={methodsFivePrinciples.contentOverviewTitle}
-          links={prinzips.map((prinzip) => ({
-            id: slugify(prinzip.Name),
-            title: `${methodsFivePrinciples.anchor.principle} ${prinzip.Name}`,
-          }))}
-        />
-      </Hero>
-
-      <div className="container my-40 space-y-96">
-        {prinzips.map((prinzip) => (
-          <Principle prinzip={prinzip} key={prinzip.Name} />
-        ))}
-      </div>
-
-      <PrinciplePosterBox hasBlueBackground={true} />
-    </>
-  );
+export function loader() {
+  return redirect(ROUTE_METHODS_PRINCIPLES.url);
 }
