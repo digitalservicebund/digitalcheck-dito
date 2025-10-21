17bccaee260b6c8f99f4bb21a44b124b0756613b
Merlin Pannewitz <merlin.pannewitz@digitalservice.bund.de>
2025-10-21 10:10:56 +0200

    Reduce snapshot test config
    Only run on chrome desktop and mobile to reduce running time.

--- tests/playwright-snapshots.config.ts
+++ tests/playwright-snapshots.config.ts
@@ -27,48 +27,10 @@ const config: PlaywrightTestConfig = {
       },
     },
     {
-      name: "Desktop Firefox",
-      use: { ...devices["Desktop Firefox"], viewport: desktopViewport },
-    },
-    {
-      name: "Desktop Safari",
-      use: { ...devices["Desktop Safari"], viewport: desktopViewport },
-    },
-    {
       name: "Mobile Chrome",
       use: {
         ...devices["Pixel 7"],
         viewport: { ...devices["Pixel 7"].viewport, height: mobileHeight },
-      },
-    },
-    {
-      name: "Mobile Safari",
-      use: {
-        ...devices["iPhone 14 Pro"],
-        viewport: {
-          ...devices["iPhone 14 Pro"].viewport,
-          height: mobileHeight,
-        },
-      },
-    },
-    {
-      name: "Tablet Chrome",
-      use: {
-        ...devices["Galaxy Tab S4 landscape"],
-        viewport: {
-          ...devices["Galaxy Tab S4 landscape"].viewport,
-          height: mobileHeight,
-        },
-      },
-    },
-    {
-      name: "Tablet Safari",
-      use: {
-        ...devices["iPad Pro 11 landscape"],
-        viewport: {
-          ...devices["iPad Pro 11 landscape"].viewport,
-          height: mobileHeight,
-        },
       },
     },
   ],
