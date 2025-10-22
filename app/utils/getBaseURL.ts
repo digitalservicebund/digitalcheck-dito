c2a5de36fa4e3f705137d868c08735610bc0c74a
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-10-22 17:52:57 +0200

    Delete getBaseURL.ts

--- app/utils/getBaseURL.ts
+++
@@ -1,7 +0,0 @@
-export default function getBaseURL(request: Request) {
-  const requestURL = new URL(request.url);
-  return requestURL.origin.replace(
-    "http://",
-    process.env.NODE_ENV === "production" ? "https://" : "http://",
-  );
-}
