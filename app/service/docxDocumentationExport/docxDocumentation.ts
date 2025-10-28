7779a2c8628b683bde0d95ef5298299eb57ccd33
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-10-28 15:45:48 +0100

    Reorder functions in docxDocumentation

--- app/service/docxDocumentationExport/docxDocumentation.ts
+++ app/service/docxDocumentationExport/docxDocumentation.ts
@@ -24,100 +24,23 @@ import {
 import strapiBlocksToDocx from "./strapiBlocksToDocx";
 const { saveAs } = fileSaver;
 
-// Renders strings with newlines as TextRuns with breaks
-const stringToTextRuns = (answer: string) =>
-  answer
-    .split("\n")
-    .map((line, idx) => new TextRun({ text: line, break: Number(idx > 0) }));
-
-const indentOptions = {
-  indent: {
-    left: convertInchesToTwip(0.5),
-  },
-};
-
-const stringToIndentParagraph = (
-  content: string | undefined,
-  options: Partial<IParagraphOptions>,
-) =>
-  new Paragraph({
-    text: content ?? "",
-    ...options,
-    ...indentOptions,
-  });
-
-const toParagraphPatch = (content?: string | Node[]): IPatch => {
-  if (content && typeof content !== "string") {
-    return {
-      type: PatchType.DOCUMENT,
-      children: strapiBlocksToDocx(content),
-    };
-  }
-  return {
-    type: PatchType.PARAGRAPH,
-    children: stringToTextRuns(content ?? ""),
-  };
-};
-
-export const buildAspectParagraphs = (
-  aspect: PrinzipAspekt,
-  reasoning?: PrincipleReasoning,
-) => [
-  stringToIndentParagraph(aspect.Titel, { heading: HeadingLevel.HEADING_2 }),
-  ...strapiBlocksToDocx(aspect.Text, indentOptions),
-  stringToIndentParagraph(documentationDocument.aspect.paragraphsLabel, {
-    style: "Textbox Label",
-  }),
-  stringToIndentParagraph(`§${reasoning?.paragraphs ?? ""}`, {
-    style: "Textbox",
-  }),
-  stringToIndentParagraph(documentationDocument.aspect.explanationLabel, {
-    style: "Textbox Label",
-  }),
-  stringToIndentParagraph(reasoning?.reason ?? "", { style: "Textbox" }),
-];
-
-const principleAnswerOptions =
-  digitalDocumentation.principlePages.radioOptions.join(" | ");
-
-const buildPrinciplePatches = (
+export default async function downloadDocumentation(
   principles: PrinzipWithAspekte[],
-  answers: DocumentationData["principles"],
-): Record<string, IPatch> =>
-  principles.reduce((acc, principle, index) => {
-    const answer = answers?.find(
-      (answer) => answer.id === principle.documentId,
+) {
+  try {
+    // Fetch the logo from the public directory
+    const template = await fetch(
+      "/documents/VORLAGE_Dokumentation_der_Digitaltauglichkeit_V2.docx",
     );
-    const positivePrincipleAnswer = answer?.answer.includes("Ja");
-    const negativePrincipleAnswer =
-      answer?.answer.includes("Nein") || answer?.answer.includes("Nicht");
-    return {
-      ...acc,
-      [`PRINCIPLE_${index + 1}_TITLE`]: toParagraphPatch(principle.Name),
-      [`PRINCIPLE_${index + 1}_DESCRIPTION`]: toParagraphPatch(
-        principle.Beschreibung,
-      ),
-      [`PRINCIPLE_${index + 1}_ANSWER`]: toParagraphPatch(
-        answer?.answer ?? principleAnswerOptions,
-      ),
-      // We always need to fill both patches to avoid the tags rendering
-      // Depending on whether the answer is positive or negative, we either fill the reasoning or the aspects
-      [`PRINCIPLE_${index + 1}_REASONING`]: toParagraphPatch(
-        negativePrincipleAnswer ? (answer?.reasoning as string) : "",
-      ),
-      [`PRINCIPLE_${index + 1}_ASPECTS`]: {
-        type: PatchType.DOCUMENT,
-        children: principle.Aspekte.flatMap((aspect, idx) =>
-          buildAspectParagraphs(
-            aspect,
-            positivePrincipleAnswer
-              ? (answer?.reasoning?.[idx] as PrincipleReasoning)
-              : {},
-          ),
-        ),
-      },
-    };
-  }, {});
+    const templateData = await template.arrayBuffer();
+    const documentationData = getDocumentationData();
+
+    const doc = await createDoc(templateData, principles, documentationData);
+    saveAs(doc, documentationDocument.filename);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 export const createDoc = (
   templateData: ArrayBuffer,
@@ -146,20 +69,93 @@ export const createDoc = (
   return patchedDocument;
 };
 
-export default async function downloadDocumentation(
-  principles: PrinzipWithAspekte[],
-) {
-  try {
-    // Fetch the logo from the public directory
-    const template = await fetch(
-      "/documents/VORLAGE_Dokumentation_der_Digitaltauglichkeit_V2.docx",
-    );
-    const templateData = await template.arrayBuffer();
-    const documentationData = getDocumentationData();
-
-    const doc = await createDoc(templateData, principles, documentationData);
-    saveAs(doc, documentationDocument.filename);
-  } catch (e) {
-    console.error(e);
+const toParagraphPatch = (content?: string | Node[]): IPatch => {
+  if (content && typeof content !== "string") {
+    return {
+      type: PatchType.DOCUMENT,
+      children: strapiBlocksToDocx(content),
+    };
   }
-}
+  return {
+    type: PatchType.PARAGRAPH,
+    // Renders strings with newlines as TextRuns with breaks
+    children: (content ?? "")
+      .split("\n")
+      .map((line, idx) => new TextRun({ text: line, break: Number(idx > 0) })),
+  };
+};
+
+const buildPrinciplePatches = (
+  principles: PrinzipWithAspekte[],
+  answers: DocumentationData["principles"],
+): Record<string, IPatch> =>
+  principles.reduce((acc, principle, index) => {
+    const answer = answers?.find(
+      (answer) => answer.id === principle.documentId,
+    );
+    const positivePrincipleAnswer = answer?.answer.includes("Ja");
+    const negativePrincipleAnswer =
+      answer?.answer.includes("Nein") || answer?.answer.includes("Nicht");
+    const principleAnswerOptions =
+      digitalDocumentation.principlePages.radioOptions.join(" | ");
+    return {
+      ...acc,
+      [`PRINCIPLE_${index + 1}_TITLE`]: toParagraphPatch(principle.Name),
+      [`PRINCIPLE_${index + 1}_DESCRIPTION`]: toParagraphPatch(
+        principle.Beschreibung,
+      ),
+      [`PRINCIPLE_${index + 1}_ANSWER`]: toParagraphPatch(
+        answer?.answer ?? principleAnswerOptions,
+      ),
+      // We always need to fill both patches to avoid the tags rendering
+      // Depending on whether the answer is positive or negative, we either fill the reasoning or the aspects
+      [`PRINCIPLE_${index + 1}_REASONING`]: toParagraphPatch(
+        negativePrincipleAnswer ? (answer?.reasoning as string) : "",
+      ),
+      [`PRINCIPLE_${index + 1}_ASPECTS`]: {
+        type: PatchType.DOCUMENT,
+        children: principle.Aspekte.flatMap((aspect, idx) =>
+          buildAspectParagraphs(
+            aspect,
+            positivePrincipleAnswer
+              ? (answer?.reasoning?.[idx] as PrincipleReasoning)
+              : {},
+          ),
+        ),
+      },
+    };
+  }, {});
+
+const indentOptions = {
+  indent: {
+    left: convertInchesToTwip(0.5),
+  },
+};
+
+const stringToIndentParagraph = (
+  content: string | undefined,
+  options: Partial<IParagraphOptions>,
+) =>
+  new Paragraph({
+    text: content ?? "",
+    ...options,
+    ...indentOptions,
+  });
+
+export const buildAspectParagraphs = (
+  aspect: PrinzipAspekt,
+  reasoning?: PrincipleReasoning,
+) => [
+  stringToIndentParagraph(aspect.Titel, { heading: HeadingLevel.HEADING_2 }),
+  ...strapiBlocksToDocx(aspect.Text, indentOptions),
+  stringToIndentParagraph(documentationDocument.aspect.paragraphsLabel, {
+    style: "Textbox Label",
+  }),
+  stringToIndentParagraph(`§${reasoning?.paragraphs ?? ""}`, {
+    style: "Textbox",
+  }),
+  stringToIndentParagraph(documentationDocument.aspect.explanationLabel, {
+    style: "Textbox Label",
+  }),
+  stringToIndentParagraph(reasoning?.reason ?? "", { style: "Textbox" }),
+];
