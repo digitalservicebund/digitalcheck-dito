c69d5414fe38b1356817614555cd51fa2d64be02
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-11-14 15:19:14 +0100

    Use HeadlessUI for AccordionItem

--- app/components/AccordionItem.tsx
+++ app/components/AccordionItem.tsx
@@ -1,4 +1,9 @@
 import { Add, Remove } from "@digitalservicebund/icons";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
 import { ReactNode } from "react";
 import { twJoin } from "tailwind-merge";
 import RichText from "~/components/RichText";
@@ -20,26 +25,32 @@ export default function AccordionItem({
   const plausibleEvent = getPlausibleEventClassName(plausibleEventName);
 
   return (
-    <details className="group border-t-2 border-t-blue-800" id={id}>
-      <summary
+    <Disclosure>
+      <DisclosureButton
         className={twJoin(
           plausibleEvent,
-          "flex w-full cursor-pointer items-center justify-between p-24 group-open:bg-blue-200 hover:bg-blue-200 focus-visible:bg-blue-200 focus-visible:outline-4 focus-visible:outline-blue-800 [&::-webkit-details-marker]:hidden",
+          "group flex w-full items-center justify-between border-t-2 border-blue-800 p-24 hover:bg-blue-200 focus-visible:bg-blue-200 focus-visible:outline-4 focus-visible:outline-blue-800 data-open:bg-blue-200",
         )}
+        id={id}
       >
         <div className="pr-10 text-left font-bold text-blue-800">
           {headline}
         </div>
-        <Add className="size-24 shrink-0 fill-blue-800 group-open:hidden" />
-        <Remove className="hidden size-24 shrink-0 fill-blue-800 group-open:block" />
-      </summary>
-      <div className="p-24">
+        <Add className="size-24 shrink-0 fill-blue-800 group-data-open:hidden" />
+        <Remove className="hidden size-24 shrink-0 fill-blue-800 group-data-open:block" />
+      </DisclosureButton>
+      <DisclosurePanel
+        className={twJoin(
+          plausibleEvent,
+          "flex w-full items-center justify-between p-24",
+        )}
+      >
         {typeof content === "string" ? (
-          <RichText markdown={content} className="ds-stack ds-stack-28" />
+          <RichText markdown={content} className="space-y-28" />
         ) : (
           content
         )}
-      </div>
-    </details>
+      </DisclosurePanel>
+    </Disclosure>
   );
 }
