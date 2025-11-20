d731caa88c1406bcbd10802309f572075a3da8ab
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-11-20 23:31:06 +0100

    Simplify routes by removing useOutletContext

--- app/routes/vorpruefung.tsx
+++ app/routes/vorpruefung.tsx
@@ -1,11 +1,9 @@
-import { Outlet, useOutletContext } from "react-router";
+import { Outlet } from "react-router";
 
 export const handle = {
   hasProgressBar: true, // enable progress bar for all child routes
 };
 
 export default function Vorpruefung() {
-  // Sets the meta title for all child pages
-  // We need to pass the Outlet context to the child routes for the feature flags to work
-  return <Outlet context={useOutletContext()} />;
+  return <Outlet />;
 }
