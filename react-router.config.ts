c433817d2c67fee294b8640b76fca0fb768dff2e
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-12-03 17:31:10 +0100

    Revert "Reapply "Try static pre-rendering for ZFL""
    This reverts commit d6e09da1d46da93f29a9f1ca59eb963bfa22b069.

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,14 +2,4 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
-  async prerender() {
-    return [
-      "/zfl",
-      "/zfl/begleitungen",
-      "/zfl/schulungen",
-      "/zfl/impressum",
-      "/zfl/datenschutz",
-      "/zfl/barrierefreiheit",
-    ];
-  },
 } satisfies Config;
