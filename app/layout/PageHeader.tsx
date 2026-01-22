eaa90672516349664847ba50be52b1e846b29166
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2026-01-22 15:24:47 +0100

    Remove Digitalcheck notice banner

--- app/layout/PageHeader.tsx
+++ app/layout/PageHeader.tsx
@@ -3,7 +3,6 @@ import PhoneOutlined from "@digitalservicebund/icons/PhoneOutlined";
 import { useEffect, useRef, useState } from "react";
 import { Link, type UIMatch, useLocation, useMatches } from "react-router";
 import { twJoin } from "tailwind-merge";
-import Banner from "~/components/Banner";
 import Container from "~/components/Container";
 import { Kopfzeile } from "~/components/kern-preview/Kopfzeile.tsx";
 import RichText from "~/components/RichText";
@@ -142,8 +141,6 @@ const PageHeader = () => {
         />
       )}
       <header className="relative" ref={headerRef}>
-        <Banner />
-
         <Kopfzeile className="relative z-30" />
         <div className="relative z-30 flex h-[72px] justify-between border-b-2 border-blue-300 bg-white pl-16 lg:px-16">
           {/* Logo and title */}
