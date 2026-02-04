838f7e9a2933c7dd18f128a2f06df142684bc641
sanni-github <giang.pham@digitalservice.bund.de>
2026-02-04 10:42:10 +0100

    Update methoden.interview-leitfaden-schritte.tsx
    added link

--- app/routes/methoden.interview-leitfaden-schritte.tsx
+++ app/routes/methoden.interview-leitfaden-schritte.tsx
@@ -11,6 +11,7 @@ import Hero from "~/components/Hero";
 import ImageBox from "~/components/ImageBox";
 import InfoBox from "~/components/InfoBox";
 import MetaTitle from "~/components/Meta";
+import NewTabLink from "~/components/NewTabLink";
 import NumberedList from "~/components/NumberedList";
 import RichText from "~/components/RichText";
 import ToC from "~/components/TableOfContentsInteractive.tsx";
@@ -308,6 +309,12 @@ export default function InterviewMethods() {
                 In mehreren Gesprächen mit einer Dauer von 45 – 60 Minuten kommen eine Menge Informationen zusammen. Im Anschluss werden die gesammelten Informationen ausgewertet. In der Regel lassen sich aus mehreren Gesprächen zentrale Muster, Herausforderungen und Erfolgsfaktoren ableiten. Nutzen Sie ggf. die [Methode Individuelle Gesprächsauswertung](https://www.oeffentliches-gestalten.de/buch/erkennen/erkenntnisse-formulieren#individuelle-gespr%C3%A4chsauswertung), um Schlüsselaspekte herauszuarbeiten.
               `}
                 />
+                 <p> Weiterführende Quelle:{" "}
+                  <NewTabLink to="https://www.orghandbuch.de/SharedDocs/faqs/Webs/OHB/DE/Methoden_Befragung/13_Durchfuehrung_muendliches_Interview.html">
+                    Orghandbuch des BMI 2026
+                  </NewTabLink>
+                 </p>
+                
               </NumberedList.Item>
             </NumberedList>
           </section>
