a93b825f7de38de492b926ce8f9d28e0edbbd7ae
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2026-03-10 19:17:55 +0100

    fix: update plausible domain

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,5 +1,5 @@
 export const PLAUSIBLE_URL = "https://plausible.io/api/event";
-export const PLAUSIBLE_DOMAIN = "erarbeiten.digitalcheck.bund.de";
+export const PLAUSIBLE_DOMAIN = "digitalcheck.bund.de";
 export const PLAUSIBLE_SCRIPT =
   "https://plausible.io/js/script.tagged-events.outbound-links.js";
 
