e6921e848437ef47f8c6429bb9aded771470bdca
Anne Ehrenpreis <anne.ehrenpreis@digitalservice.bund.de>
2026-03-11 17:52:40 +0100

    refactor(separator): Separator component to accept custom className prop

--- app/components/Separator.tsx
+++ app/components/Separator.tsx
@@ -1,3 +1,16 @@
-export default function Separator() {
-  return <hr className="border-0 border-b-2 border-solid border-gray-400" />;
+import customTwMerge from "~/utils/tailwindMerge";
+
+type SeperatorProps = {
+  className?: string;
+};
+
+export default function Separator({ className }: Readonly<SeperatorProps>) {
+  return (
+    <hr
+      className={customTwMerge(
+        "border-0 border-b-2 border-solid border-gray-400",
+        className,
+      )}
+    />
+  );
 }
