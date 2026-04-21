caa6d2f2c206c84e34d9eaac0c1295a13dd974b7
MasterCarl <MasterCarl@users.noreply.github.com>
2026-04-21 12:45:39 +0000

    Deploying to gh-pages from @ digitalservicebund/digitalcheck-dito@bdcd3b71b46cd690bb339e11da192f0d8eb08d50 🚀

---
+++ assets/route-Df6pSYky.js
@@ -0,0 +1,1 @@
+import{w as a,j as t,O as e,d as n}from"./chunk-OE4NN4TA-B87l8SeB.js";import{D as r}from"./DocumentationDataProvider-CQ22XA4h.js";const i={hasProgressBar:!0};function m(){return n("routes/dokumentation")}const c=a(function(){return t.jsx(r,{children:t.jsx(e,{})})});export{i as h,c as r,m as u};
