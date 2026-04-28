bb531da1bc735f270381a85b6b19e2c32c5d3c5b
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-04-28 14:19:06 +0200

    chore(deps): bump dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,19 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     !process.env.VITEST && reactRouter(),
     tailwindcss(),
-    tsconfigPaths(),
     devtoolsJson(),
   ],
   build: {
     target: "ES2022",
+    cssTarget: "chrome100",
     rollupOptions: {
       external: [/\.spec\.tsx?$/], // exclude test files from production build
     },
