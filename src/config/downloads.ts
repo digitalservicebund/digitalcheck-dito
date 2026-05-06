3ab3a180558d0162201d03a00439ca0b1b8f9f18
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-05-06 19:54:33 +0200

    feat: add basePath in download routes for preview
    ZFL-105

--- src/config/downloads.ts
+++ src/config/downloads.ts
@@ -5,32 +5,37 @@ export type DownloadRoute = {
   readonly filename: string;
 };
 
+const basePath =
+  import.meta.env?.PUBLIC_STAGE === "preview"
+    ? import.meta.env?.BASE_URL || ""
+    : "";
+
 export const prinzipienPoster: DownloadRoute = {
-  path: "/documents/Prinzipien-Poster.pdf",
+  path: `${basePath}/documents/Prinzipien-Poster.pdf`,
   filename: "Prinzipien-Poster.pdf",
 };
 
 export const anleitungFlussdiagramm: DownloadRoute = {
-  path: "/documents/Anleitung_Flussdiagramm_erstellen.pptx",
+  path: `${basePath}/documents/Anleitung_Flussdiagramm_erstellen.pptx`,
   filename: "Anleitung_Flussdiagramm_erstellen.pptx",
 };
 
 export const dokumentationStaticWordV1: DownloadRoute = {
-  path: "/documents/Dokumentieren-der-Digitaltauglichkeit_V1-5-1.docx",
+  path: `${basePath}/documents/Dokumentieren-der-Digitaltauglichkeit_V1-5-1.docx`,
   filename: "Dokumentieren-der-Digitaltauglichkeit.docx",
 };
 
 export const dokumentationStaticWordV2: DownloadRoute = {
-  path: "/documents/Dokumentation_der_Digitaltauglichkeit_V2.docx", // TODO: use dynamic generation
+  path: `${basePath}/documents/Dokumentation_der_Digitaltauglichkeit_V2.docx`, // TODO: use dynamic generation
   filename: "Dokumentation_der_Digitaltauglichkeit.docx",
 };
 
 export const dokumentationTemplateWordV1: DownloadRoute = {
-  path: "/documents/VORLAGE_Dokumentation_der_Digitaltauglichkeit_V1.docx",
+  path: `${basePath}/documents/VORLAGE_Dokumentation_der_Digitaltauglichkeit_V1.docx`,
   filename: "Dokumentieren-der-Digitaltauglichkeit.docx",
 };
 
 export const dokumentationTemplateWordV2: DownloadRoute = {
-  path: "/documents/VORLAGE_Dokumentation_der_Digitaltauglichkeit_V2.docx",
+  path: `${basePath}/documents/VORLAGE_Dokumentation_der_Digitaltauglichkeit_V2.docx`,
   filename: "Dokumentation_der_Digitaltauglichkeit.docx",
 };
