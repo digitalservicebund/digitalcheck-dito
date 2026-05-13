9f9edf5daba030f8778a3a2beb72f94b7da70662
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-05-13 09:37:15 +0200

    test: upd ts/vite/playwright config for Astro
    ZFL-105

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,30 @@
-import { defineConfig } from "vitest/config";
-import viteConfig from "./vite.config";
+/// <reference types="vitest/config" />
+import { getViteConfig } from "astro/config";
 
-export default defineConfig({
-  ...viteConfig,
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["vitest-setup.ts"],
-    include: ["app/**/*.{test,spec}.ts?(x)"],
-    chaiConfig: {
-      truncateThreshold: 200,
+export const baseUrl = "https://example.com";
+
+export default getViteConfig(
+  {
+    test: {
+      chaiConfig: {
+        truncateThreshold: 200,
+      },
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["vitest-setup.ts"],
+      include: [
+        "**/*.test.{ts,tsx}",
+        "**/*.spec.{ts,tsx}",
+        "!tests/**",
+        "!**/*.browser.test.{ts,tsx}",
+      ],
+      typecheck: {
+        tsconfig: "./tsconfig.test.json",
+      },
     },
   },
-});
+  {
+    site: baseUrl,
+    trailingSlash: "always",
+  },
+);
