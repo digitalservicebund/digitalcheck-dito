cc17261613d65edb95c781b6149d234211ca5f93
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-05-13 17:54:44 +0200

    chore: react-router cleanup

--- app/routes/grundlagen._index.tsx
+++ app/routes/grundlagen._index.tsx
@@ -1,11 +1,1 @@
-import { methoden_fuenfPrinzipien } from "@/config/routes";
-import { Navigate } from "react-router";
-import { createRedirectLoader } from "~/utils/redirectLoader";
-
-const to = methoden_fuenfPrinzipien.path;
-
-export const loader = createRedirectLoader(to);
-
-export default function Redirect() {
-  return <Navigate to={to} replace />;
-}
+// redirect moved to astro.config.mjs
