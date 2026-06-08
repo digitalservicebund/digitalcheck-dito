4eb4e45f0840a13ebc8b6e5ca0def056be6c9e85
daniel-marcus <daniel-marcus@users.noreply.github.com>
2026-06-08 17:40:09 +0000

    Deploying to gh-pages from @ digitalservicebund/digitalcheck-dito@669ba806bf90e03aa4d1000f5d191b9c6c2ef819 🚀

---
+++ previews/laender-astro/_astro/additionalRoutes.ofwnChhG.js
@@ -0,0 +1,1 @@
+import{u as e,o as n}from"./routes.CCVuEwKG.js";import{g as t}from"./tabs.Q53h5zdm.js";const s=e.path+t("online-schulungen","angebote"),r=n.path+"/regelungen";export{s as R,r as a};
---
+++ previews/laender-astro/_astro/dokumentation._documentationNavigation.hinweise.DEc8GVGw.js
@@ -0,0 +1,14 @@
+import{j as e}from"./tailwindMerge.gN6BWK24.js";import{s as o}from"./routes.CCVuEwKG.js";import{r as l}from"./index.Ds7yq6at.js";import{B as c,L as u}from"./Button.fvDX-KZB.js";import{B as d}from"./ButtonContainer.CUkDFbqX.js";import{M as h,H as m}from"./Meta.BHpGBQkm.js";import{R as g}from"./RichText.BhR-Aseg.js";import{g as p}from"./general.BLhXOskb.js";import{u as x,D as b}from"./DocumentationPageShell.BOICgt_t.js";const k=`
+## Datenspeicherung
+
+Ihre eingegebenen Daten werden **unbegrenzt lange** in der Sitzung gespeichert, bis
+
+a) Sie eine neue Dokumentation starten, oder<br />
+b) Ihr SINA-Rechner die Daten löscht (falls das bei Ihnen voreingestellt ist)
+
+**Bitte beachten Sie:** Es ist jeweils nur die Bearbeitung einer Dokumentation möglich. Sie können den Vorgang unterbrechen und später fortsetzen. Der aktuelle Stand bleibt auch nach Schließen des Fensters erhalten.
+
+## Zwischenspeicherung als Word-Dokument
+
+Zur externen Weiterbearbeitung, internen Abstimmung oder für Änderungen können Sie den aktuellen Stand Ihrer Angaben als Word-Dokument exportieren und lokal speichern. Die finale Datei können Sie zu jedem Zeitpunkt per E-Mail an den NKR versenden.
+`;function i(){const{nextUrl:n,previousUrl:t}=x(),[r,s]=l.useState(!1);return e.jsxs(e.Fragment,{children:[e.jsx(h,{prefix:`Dokumentation: ${o.title}`}),e.jsx(m,{text:"Wichtige Hinweise",tagName:"h1",look:"ds-heading-02-reg",className:"mb-16"}),e.jsx(g,{className:"[&>h2]:ds-heading-03-reg [&>h2]:mt-40",markdown:k}),e.jsxs("label",{className:"ds-label-01-reg flex flex-row items-center gap-16",children:[e.jsx("input",{type:"checkbox",className:"ds-checkbox",checked:r,onChange:a=>s(a.target.checked)}),"Ich habe die oberen Hinweise gelesen."]}),e.jsxs(d,{children:[e.jsx(c,{look:"primary",disabled:!r,type:"button",onClick:()=>globalThis.location.href=n,children:"Verstanden und weiter"}),e.jsx(u,{href:t,look:"tertiary",children:p.buttonBack.text})]})]})}function R(){return e.jsx(i,{})}function v({prinzips:n,currentUrl:t}){return e.jsx(b,{prinzips:n,currentUrl:t,children:e.jsx(i,{})})}export{i as DocumentationHinweise,v as HinweisePage,R as default};
