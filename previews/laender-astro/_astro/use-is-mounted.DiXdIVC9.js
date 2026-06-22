d0e7f73ecb1e506016bdecb81d31183b5eec505d
HendrikSchmidt <HendrikSchmidt@users.noreply.github.com>
2026-06-22 14:31:37 +0000

    Deploying to gh-pages from @ digitalservicebund/digitalcheck-dito@4e963969a93d3351639449ae16d1e767b1946515 🚀

---
+++ previews/laender-astro/_astro/use-is-mounted.DiXdIVC9.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.Ds7yq6at.js";import{n as t}from"./open-closed.B3V-tccQ.js";function n(){let r=e.useRef(!1);return t(()=>(r.current=!0,()=>{r.current=!1}),[]),r}export{n as f};
