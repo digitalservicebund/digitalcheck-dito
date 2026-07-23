afbf86009b65114909f9fc6e5189e9da4cb9f9aa
Anne Ehrenpreis <anne.ehrenpreis@digitalservice.bund.de>
2026-07-23 16:37:50 +0200

    feat(input): enhance styling for warning state for all form components

--- src/components/InputError.tsx
+++ src/components/InputError.tsx
@@ -9,23 +9,43 @@ type InputErrorProps = PropsWithChildren<{
 
 const InputError = ({ id, children, look = "error" }: InputErrorProps) => {
   return (
-    <div
-      aria-live="assertive"
-      role="alert"
-      id={id}
-      data-testid={id}
-      className={twMerge(
-        "text-ds-error flex items-center gap-x-4",
-        look === "warning" && "bg-ds-yellow-300 inline-flex px-4 text-black",
+    <div>
+      {look === "warning" && (
+        <p className="kern-error" id={id}>
+          <span
+            className={twMerge(
+              "kern-icon kern-icon--warning kern-icon--md",
+              "!bg-ds-yellow-700",
+            )}
+            aria-hidden="true"
+          ></span>
+          <span className="kern-body">{children}</span>
+        </p>
       )}
-    >
       {look === "error" && (
         <div className="shrink-0">
           <ErrorOutline className="fill-ds-error" />
         </div>
       )}
-      <span className="sr-only">Fehler:</span> {children}
     </div>
+
+    // <div
+    //   aria-live="assertive"
+    //   role="alert"
+    //   id={id}
+    //   data-testid={id}
+    //   className={twMerge(
+    //     "text-ds-error flex items-center gap-x-4",
+    //     look === "warning" && "bg-ds-yellow-300 inline-flex px-4 text-black",
+    //   )}
+    // >
+    // {look === "error" && (
+    //   <div className="shrink-0">
+    //     <ErrorOutline className="fill-ds-error" />
+    //   </div>
+    // )}
+    //   <span className="sr-only">Fehler:</span> {children}
+    // </div>
   );
 };
 
